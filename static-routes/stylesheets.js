93d3fac7927ceb04bcbee10f08d1d3f843f8fd40
Bjørge Næss <bjoerge@gmail.com>
2015-03-23 16:56:25 +0100

    Misc. cleanup

--- static-routes/stylesheets.js
+++ static-routes/stylesheets.js
@@ -1,24 +1,24 @@
-var sass = require('node-sass');
-var path = require('path');
-var config = require('../config');
-var fs = require('fs');
-var imdino = require('imdi-no');
+const sass = require('node-sass');
+const path = require('path');
+const config = require('../config');
+const fs = require('fs');
+const imdino = require('imdi-no');
 
-function toBase64(str) {
-  return new Buffer(str).toString('base64');
-}
+const development = config.env === 'development';
 
 module.exports = {
   "/stylesheets/main.css": function (callback) {
-    var opts = {
-      file: path.join(__dirname, "/../stylesheets/main.scss"),
-      outFile: '/stylesheets/main.css',
-      sourceMap: config.env === 'development',
-      sourceMapEmbed: config.env === 'development',
-      sourceMapContents: true,
-      sourceComments: config.env === 'development',
-      outputStyle: config.env === 'development' ? 'nested' : 'compressed',
-      includePaths: imdino.includePaths
+
+    const opts = {
+      file:               path.join(__dirname, "/../stylesheets/main.scss"),
+      outFile:            '/stylesheets/main.css',
+      includePaths:       imdino.includePaths,
+
+      sourceMap:          development,
+      sourceMapEmbed:     development,
+      sourceMapContents:  development,
+      sourceComments:     development,
+      outputStyle:        development ? 'nested' : 'compressed'
     };
 
     sass.render(opts, (err, result)=> {

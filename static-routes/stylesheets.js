11c8b12428876c846f53d7ed068a765a7a4b6316
Bjørge Næss <bjoerge@gmail.com>
2015-03-23 14:44:49 +0100

    Include imdi-no as a dependency

--- static-routes/stylesheets.js
+++ static-routes/stylesheets.js
@@ -1,6 +1,7 @@
 var sass = require('node-sass');
 var path = require('path');
 var config = require('../config');
+var imdino = require('imdi-no');
 
 function toBase64(str) {
   return new Buffer(str).toString('base64');
@@ -16,8 +17,8 @@ module.exports = {
       sourceMapContents: true,
       sourceComments: config.env === 'development',
       omitSourceMapUrl: true,
-      outputStyle: config.env === 'development' ? 'nested' : 'compressed'
-
+      outputStyle: config.env === 'development' ? 'nested' : 'compressed',
+      includePaths: imdino.includePaths
     };
     sass.render(opts, function (err, result) {
         if (err) {
@@ -28,13 +29,7 @@ module.exports = {
           return callback(null, result.css);
         }
 
-        result.map.sources = result.map.sources.map(function (source) {
-          // Set correct path of each source file
-          return '../' + source;
-        });
-
         var comment = "/*# sourceMappingURL=data:application/json;base64," + toBase64(JSON.stringify(result.map)) + "*/";
-
         callback(null, result.css + "\n" + comment);
       }
     );

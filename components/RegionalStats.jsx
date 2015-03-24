6a18bc6827b650dbd3fad56af5bd6d6058868814
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2015-03-24 16:36:17 +0100

    adding preamble

--- components/RegionalStats.jsx
+++ components/RegionalStats.jsx
@@ -8,6 +8,7 @@ module.exports = React.createClass({
 
     return (
       <div className="imdikator-list__wrapper">
+        <p className="imdikator-preamble">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium quam vel est vulputate, quis accumsan risus tincidunt. Suspendisse bibendum placerat laoreet. In imperdiet in neque quis dignissim. Phasellus quis magna aliquet, euismod nulla eu, viverra lectus. Phasellus vehicula ultricies laoreet. Etiam at rhoncus elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
         <Groups groupData={this.props.groupData} />
       </div>
     )

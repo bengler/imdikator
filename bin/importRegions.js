e162b5a079081a759783ac51e0089ffe024cb1cb
Bjørge Næss <bjoerge@gmail.com>
2015-03-26 14:17:47 +0100

    Import bydeler too

--- bin/importRegions.js
+++ bin/importRegions.js
@@ -8,9 +8,11 @@ const writeFile = Bluebird.promisify(require('fs').writeFile);
 
 const API_URL_KOMMUNE = "http://hotell.difi.no/api/json/ssb/regioner/kommuner";
 const API_URL_FYLKE = "http://hotell.difi.no/api/json/ssb/regioner/fylke";
+const API_URL_BYDEL = "http://hotell.difi.no/api/json/ssb/regioner/bydel";
 
 const OUT_FILE_KOMMUNE = "./data/kommuner.json";
 const OUT_FILE_FYLKE = "./data/fylker.json";
+const OUT_FILE_BYDEL = "./data/bydeler.json";
 
 function pageFetcher(url) {
   return function fetchPage(page) {
@@ -23,11 +25,11 @@ function pageFetcher(url) {
   };
 }
 
-const fetchPage = pageFetcher(API_URL_KOMMUNE);
+const fetchKommunePage = pageFetcher(API_URL_KOMMUNE);
 
-const fetchKommuner = fetchPage(1)
+const fetchKommuner = fetchKommunePage(1)
   .then(firstPage => {
-    return Bluebird.map(range(2, firstPage.pages+1), fetchPage).then(pages => [firstPage, ...pages]);
+    return Bluebird.map(range(2, firstPage.pages+1), fetchKommunePage).then(pages => [firstPage, ...pages]);
   })
   .then(allPages => {
     return allPages
@@ -39,6 +41,23 @@ const fetchKommuner = fetchPage(1)
     return writeFile(OUT_FILE_KOMMUNE, JSON.stringify(entries, null, 2))
   });
 
+
+const fetchBydelPage = pageFetcher(API_URL_BYDEL);
+
+const fetchBydeler = fetchBydelPage(1)
+  .then(firstPage => {
+    return Bluebird.map(range(2, firstPage.pages+1), fetchBydelPage).then(pages => [firstPage, ...pages]);
+  })
+  .then(allPages => {
+    return allPages
+      .map(page => page.entries)
+      .map(entries => entries.map(entry => pick(entry, 'tittel', 'kode')))
+      .reduce((acc, entries) => acc.concat(entries), [])
+  })
+  .tap(entries => {
+    return writeFile(OUT_FILE_BYDEL, JSON.stringify(entries, null, 2))
+  });
+
 const fetchFylke = request(API_URL_FYLKE)
     .then(([response, body]) => {
       const entries = JSON.parse(body).entries;
@@ -46,14 +65,16 @@ const fetchFylke = request(API_URL_FYLKE)
     })
     .tap(entries => {
       return writeFile(OUT_FILE_FYLKE, JSON.stringify(entries, null, 2))
-    })
-  ;
+    });
 
-Bluebird.join(fetchKommuner, fetchFylke)
-  .then(([kommuner, fylker])=> {
-    console.log(`Imported ${kommuner.length} kommuner and ${fylker.length} fylker.`);
+Bluebird
+  .join(fetchKommuner, fetchFylke, fetchBydeler)
+  .then(([kommuner, fylker, bydeler])=> {
     console.log(`Done!`);
+    console.log(`  Imported ${fylker.length} fylker.`);
+    console.log(`  Imported ${kommuner.length} kommuner.`);
+    console.log(`  Imported ${bydeler.length} bydeler.`);
   })
   .catch(e => {
     throw e;
-  })
+  });

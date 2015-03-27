c379031e48a339ac8ff790e367bfe7040ee7cddc
Bjørge Næss <bjoerge@gmail.com>
2015-03-27 15:24:25 +0100

    Load chart data in renderChart instead

--- components/charts/Area.jsx
+++ components/charts/Area.jsx
@@ -7,29 +7,30 @@ module.exports = React.createClass({
 
   },
   componentDidMount() {
-    const groups = this.props.data.map(d => d[0]);
-    this.renderChart();
     this.chart = c3.generate({
       bindto: this.getDOMNode(),
       data: {
-        columns: this.props.data,
-        types: groups.reduce((types, g) => {
-          types[g] = 'area'
-          // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
-          return types;
-        }, {}),
-        groups: [groups]
+        columns: []
       }
     });
+    this.renderChart();
   },
   componentDidUpdate() {
     this.renderChart();
   },
   renderChart() {
-
+    const groups = this.props.data.map(d => d[0]);
+    this.chart.load({
+      columns: this.props.data,
+      types: groups.reduce((types, g) => {
+        types[g] = 'area'
+        // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
+        return types;
+      }, {}),
+      groups: [groups]
+    })
   },
   render() {
-    console.log(this.props.data)
     return <div/>
   }
 });

c5c0b7f4515c8cf8a1c2c68d4fe86ea85d3b8b6b
Bjørge Næss <bjoerge@gmail.com>
2015-04-25 15:42:51 +0200

    Make start task call bin/server.js instead

---
+++ bin/server.js
@@ -0,0 +1,10 @@
+const app = require("../app");
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+
+});

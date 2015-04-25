f5b416c1d19dc628dd9dd509d17dadb662fa216c
Even Westvang <even@bengler.no>
2015-04-25 21:12:35 +0200

    Unused table finder

---
+++ bin/findUnusedTables.js
@@ -0,0 +1,89 @@
+console.info("Om nom nom");
+
+const dotty = require("dotty");
+const groups = require('../data/groups.json');
+const tableMetadata = require('../data/tables.json');
+const data = require('../dataset/tree.json');
+
+let tablesInUse = [];
+
+groups.forEach((g) => {
+	g.items.forEach((t) => {
+		tablesInUse[t.table] = t;
+	})
+});
+
+const allTables = Object.keys(data);
+let outstandingTables = allTables.filter((t)=> {
+	return !tablesInUse[t];
+});
+
+console.info("Tables not in use");
+console.info(outstandingTables);
+console.info("----")
+
+// outstandingTables = [outstandingTables[4]];
+
+let tableDescriptions = [];
+
+outstandingTables.forEach((tableName)=> {
+
+	let table = data[tableName];
+	let tableDimensions = deletePlaceNames(byFirstKey(byFirstKey(table)));
+
+	let variables = [];
+	searchTable(tableDimensions, variables, 0);
+
+	// innvkat_3:alle; utd_4: grunnskole, vgs, universitet_og_hogskole, uoppgitt_ingen; kjonn:0,1
+
+	let specString = variables.map( (v) => {
+		return v.label + ":" + v.include.join(",");
+	}).join(";")
+
+
+	tableDescriptions.push({
+		table: tableName,
+		specString: specString
+		// variables: variables
+	})
+
+});
+
+console.info(tableDescriptions);
+
+
+function searchTable(tableDimensions, variables, level) {
+	if (typeof tableDimensions === "string") {
+		return
+	}
+
+	let spec = variables[level];
+	if (spec === undefined) {
+		spec = variables[level] = {
+			label: "",
+			include: []
+		}		
+	}
+
+	spec.label = Object.keys(tableDimensions)[0]
+	let categories = Object.keys(tableDimensions[spec.label])
+
+	// spec.include = spec.include.concat(categories);
+	spec.include = categories;
+
+	variables[level] = spec;
+
+	categories.forEach((c)=> {
+		searchTable(tableDimensions[spec.label][c], variables, level + 1);
+	});
+}
+
+function deletePlaceNames(obj) {
+	const deleteMe = ["bydel_nr", "fylke_nr", "fylke_navn", "kommune_nr", "Kommune_navn"];
+	deleteMe.forEach((d)=> delete obj[d]);
+	return obj;
+}
+
+function byFirstKey(tree) {
+	return tree[Object.keys(tree)[0]];
+}

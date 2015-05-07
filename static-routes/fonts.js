2332a5ac79f8c9cc2e53271ae792db3e1c220f8e
Bjørge Næss <bjoerge@gmail.com>
2015-05-07 14:59:52 +0200

    Fix fonts for deploy too

--- static-routes/fonts.js
+++ static-routes/fonts.js
@@ -9,10 +9,15 @@ const paths = [
 
 paths.forEach(p => {
   const srcDir = path.join(imdino.paths.fonts, p);
-  fs.readdirSync(srcDir).forEach(file => {
-    const srcPath = path.join(srcDir, file);
-    module.exports[path.join('/fonts', p, file)] = function () {
-      return fs.createReadStream(srcPath);
-    }
-  });
+  fs.readdirSync(srcDir)
+    .forEach(file => {
+      const srcPath = path.join(srcDir, file);
+      if (fs.statSync(srcPath).isDirectory()) {
+        return;
+      }
+      console.log("Mapping ", path.join('/fonts', p, file), ' => ', srcPath)
+      module.exports[path.join('/fonts', p, file)] = function () {
+        return fs.createReadStream(srcPath);
+      }
+    });
 })

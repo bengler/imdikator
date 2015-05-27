666f0ac358552020600e6e7ac504b328af3c3ecf
Bjørge Næss <bjoerge@gmail.com>
2015-05-27 14:55:13 +0200

    Set correct key on ItemDataFetcher

--- components/Groups.jsx
+++ components/Groups.jsx
@@ -6,13 +6,28 @@ const Heading = require("./Heading.jsx");
 module.exports = React.createClass({
   displayName: 'Groups',
   render() {
-    const charts = this.props.statGroup.items.map( (item, i)=> {
-      return <Chart id={i} urlName={this.props.statGroup.urlName} title={item.title} key={i} item={item} regions={this.props.regions} municipality={this.props.municipality}/>
+    const charts = this.props.statGroup.items.map( (item,i)=> {
+      const key = [
+        item.chartKind,
+        item.table,
+        item.dimensions.map(dim => dim.label + [].concat(dim.include).concat(dim.exclude).join(".")).join("-"),
+        item.time]
+        .join('/');
+      return (
+        <Chart
+          urlName={this.props.statGroup.urlName}
+          title={item.title}
+          key={key}
+          item={item}
+          regions={this.props.regions}
+          municipality={this.props.municipality}
+        />
+      )
     })
     return (
       <div className="wrapper imdikator-groups">
         <div className="grid">
-          {charts}  
+          {charts}
         </div>
       </div>
     )

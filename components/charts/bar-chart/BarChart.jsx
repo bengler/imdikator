cbbe2ef2124d5a9930cf8eedce01fe7c47cc5031
Rune Botten <rbotten@gmail.com>
2015-08-25 17:30:03 +0200

    Example of scaling the bar chart

--- components/charts/bar-chart/BarChart.jsx
+++ components/charts/bar-chart/BarChart.jsx
@@ -4,57 +4,41 @@ import D3Chart from '../../utils/D3Chart'
 
 const sampleData = [10, 15, 5, 30]
 
-/**
- * Only for development
- */
 export default class BarChart extends React.Component {
   drawPoints(el, scales, data) {
 
+    const width  = el.offsetWidth
+    const height = el.offsetHeight
+
+    // Make scaling functions for x-position and height
+    // domain is the range of input data and the range is the mapping range for a value
+    // within that domain
+    const xPos = d3.scale.linear().domain([0, sampleData.length]).range([0, width])
+    const hVal = d3.scale.linear().domain([0, d3.max(sampleData)]).range([0, height])
+     
+    const padding = 10
+
     const g = d3.select(el).selectAll('.d3-points')
-
-    const point = g.selectAll('.bar')
-      .data(data)
-
+    const point = g.selectAll('.bar').data(data)
     point.enter()
       .append('rect')
       .attr('class', 'bar')
       .attr({
-        x: (d, i) => i * 12,
-        y: d => 200 - d,
-        height: d => d,
-        width: 10
+        x: (d, i) => xPos(i),
+        y: d => el.offsetHeight - hVal(d),
+        height: d => hVal(d),
+        width: (d, i) => xPos(1) - padding
       })
-    .style('fill', 'blue')
-
-    // EXIT
     point.exit().remove()
   }
 
+  // Not using this func here
   scales(el, domain) {
-    if (!domain) {
-      return null
-    }
-
-    const width = el.offsetWidth
-    const height = el.offsetHeight
-
-    const x = d3.scale.linear()
-      .range([0, width])
-      .domain(domain.x)
-
-    const y = d3.scale.linear()
-      .range([height, 0])
-      .domain(domain.y)
-
-    const z = d3.scale.linear()
-      .range([5, 20])
-      .domain([1, 10])
-
-    return {x: x, y: y, z: z}
   }
+
   render() {
     return (
-      <D3Chart data={sampleData} domain={{x: [0, 30], y: [0, 100]}} drawPoints={this.drawPoints} scales={this.scales}/>
+      <D3Chart data={sampleData} drawPoints={this.drawPoints} scales={this.scales}/>
     )
   }
 

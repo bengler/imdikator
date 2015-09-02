7ceda7f4c0eb133d38c301d2548c2fb7a1f24f5f
Rune Botten <rbotten@gmail.com>
2015-09-02 17:56:49 +0200

    Experiment with removing Y axis on PyramideChart and showing axis for every dimention on the categories

--- components/charts/pyramid-chart/PyramidChart.jsx
+++ components/charts/pyramid-chart/PyramidChart.jsx
@@ -3,41 +3,42 @@ import d3 from 'd3'
 import D3Chart from '../../utils/D3Chart'
 
 const sampleData = [
-  {category: 'Hele befolkningen', series: 'Menn', dimention: '0-18', value: 50000},
-  {category: 'Hele befolkningen', series: 'Menn', dimention: '19-30', value: 128000},
-  {category: 'Hele befolkningen', series: 'Menn', dimention: '30-50', value: 566000},
-  {category: 'Hele befolkningen', series: 'Menn', dimention: '50-80', value: 11000},
-  {category: 'Hele befolkningen', series: 'Menn', dimention: '80+', value: 23000},
+  {category: 'Hele befolkningen', series: 'Menn', dimention: '0-18', value: 700000},
+  {category: 'Hele befolkningen', series: 'Menn', dimention: '19-30', value: 800000},
+  {category: 'Hele befolkningen', series: 'Menn', dimention: '30-50', value: 900000},
+  {category: 'Hele befolkningen', series: 'Menn', dimention: '50-80', value: 600000},
+  {category: 'Hele befolkningen', series: 'Menn', dimention: '80+', value: 200000},
 
-  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '0-18', value: 45000},
-  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '19-30', value: 98000},
-  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '30-50', value: 620000},
-  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '50-80', value: 150000},
-  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '80+', value: 52500},
+  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '0-18', value: 700000},
+  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '19-30', value: 800000},
+  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '30-50', value: 900000},
+  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '50-80', value: 600000},
+  {category: 'Hele befolkningen', series: 'Kvinner', dimention: '80+', value: 200000},
 
-  {category: 'Innvandrere', series: 'Menn', dimention: '0-18', value: 12800},
-  {category: 'Innvandrere', series: 'Menn', dimention: '19-30', value: 32000},
-  {category: 'Innvandrere', series: 'Menn', dimention: '30-50', value: 66000},
-  {category: 'Innvandrere', series: 'Menn', dimention: '50-80', value: 18000},
-  {category: 'Innvandrere', series: 'Menn', dimention: '80+', value: 4500},
+  {category: 'Innvandrere', series: 'Menn', dimention: '0-18', value: 400000},
+  {category: 'Innvandrere', series: 'Menn', dimention: '19-30', value: 500000},
+  {category: 'Innvandrere', series: 'Menn', dimention: '30-50', value: 600000},
+  {category: 'Innvandrere', series: 'Menn', dimention: '50-80', value: 100000},
+  {category: 'Innvandrere', series: 'Menn', dimention: '80+', value: 40000},
 
-  {category: 'Innvandrere', series: 'Kvinner', dimention: '0-18', value: 12800},
-  {category: 'Innvandrere', series: 'Kvinner', dimention: '19-30', value: 32000},
-  {category: 'Innvandrere', series: 'Kvinner', dimention: '30-50', value: 66000},
-  {category: 'Innvandrere', series: 'Kvinner', dimention: '50-80', value: 18000},
-  {category: 'Innvandrere', series: 'Kvinner', dimention: '80+', value: 4500},
+  {category: 'Innvandrere', series: 'Kvinner', dimention: '0-18', value: 400000},
+  {category: 'Innvandrere', series: 'Kvinner', dimention: '19-30', value: 500000},
+  {category: 'Innvandrere', series: 'Kvinner', dimention: '30-50', value: 600000},
+  {category: 'Innvandrere', series: 'Kvinner', dimention: '50-80', value: 100000},
+  {category: 'Innvandrere', series: 'Kvinner', dimention: '80+', value: 40000},
 
-  {category: 'Befolkningen ellers', series: 'Menn', dimention: '0-18', value: 12800},
-  {category: 'Befolkningen ellers', series: 'Menn', dimention: '19-30', value: 32000},
-  {category: 'Befolkningen ellers', series: 'Menn', dimention: '30-50', value: 66000},
-  {category: 'Befolkningen ellers', series: 'Menn', dimention: '50-80', value: 18000},
-  {category: 'Befolkningen ellers', series: 'Menn', dimention: '80+', value: 4500},
+  {category: 'Befolkningen ellers', series: 'Menn', dimention: '0-18', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Menn', dimention: '19-30', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Menn', dimention: '30-50', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Menn', dimention: '50-80', value: 500000},
+  {category: 'Befolkningen ellers', series: 'Menn', dimention: '80+', value: 160000},
 
-  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '0-18', value: 12800},
-  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '19-30', value: 32000},
-  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '30-50', value: 66000},
-  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '50-80', value: 18000},
-  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '80+', value: 4500},
+  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '0-18', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '19-30', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '30-50', value: 300000},
+  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '50-80', value: 500000},
+  {category: 'Befolkningen ellers', series: 'Kvinner', dimention: '80+', value: 160000},
+
 ]
 
 export default class PyramidChart extends React.Component {
@@ -59,7 +60,7 @@ export default class PyramidChart extends React.Component {
 
     // X axis scale for series
     const x1 = d3.scale.ordinal()
-    x1.domain(seriesNames).rangeRoundBands([0, x0.rangeBand()], 0.1)
+    x1.domain(seriesNames).rangeRoundBands([0, x0.rangeBand()], 0, 0.1)
 
     // X axis scale for values in a series
     const x2 = d3.scale.linear()
@@ -72,27 +73,13 @@ export default class PyramidChart extends React.Component {
     .attr('class', 'axis')
     .attr('transform', 'translate(0, ' + this.size.height + ')')
     .call(xAxis)
+    .selectAll('path, .tick > line')
+    .style('display', 'none')
     .selectAll('.tick text')
-      .call(this.wrapTextNode, x0.rangeBand())
+    .call(this.wrapTextNode, x0.rangeBand())
 
     // Y axis scale for dimentions
-    const y0 = d3.scale.ordinal().domain(dimentions).rangeRoundBands([this.size.height, 0], 0.25)
-
-    // Add the y axis
-    const yAxis = d3.svg.axis().scale(y0).orient('left')
-    svg.append('g')
-    .attr('class', 'axis')
-    .call(yAxis)
-
-    svg.append('g')
-    .attr('class', 'axis')
-    .call(yAxis)
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', '.5em')
-    .attr('dy', '.71em')
-    .style('text-anchor', 'end')
-    .text('Aldersgruppe') // TODO: Get this unit from data
+    const y0 = d3.scale.ordinal().domain(dimentions).rangeRoundBands([this.size.height, 0], 0.25, 0.5)
 
     const category = svg.selectAll('.category')
     .data(entries)
@@ -103,12 +90,39 @@ export default class PyramidChart extends React.Component {
     const series = category.selectAll('.series')
     .data(d => d.values)
     .enter().append('g')
-    .attr('class', dataItem => 'series')
+    .attr('class', d => {
+      if (seriesNames.indexOf(d.key) == 0) {
+        return 'series left'
+      }
+      return 'series right'
+    })
     .attr('alt', dataItem => dataItem.key)
     .attr('transform', (dataItem, index) => {
       const xTranslate = x1(dataItem.key)
       return 'translate(' + xTranslate + ',0)'
     })
+
+    // Add axis to all series
+    const sx0 = d3.scale.linear()
+    sx0.domain(minMax).range([0, x1.rangeBand()])
+    const rightSeriesAxis = d3.svg.axis().scale(sx0).orient('bottom')
+    rightSeriesAxis.tickValues(minMax)
+    svg.selectAll('.series.right')
+    .append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0, ' + (this.size.height - 20) + ')')
+    .call(rightSeriesAxis)
+
+    // Left
+    const sx1 = d3.scale.linear()
+    sx1.domain(minMax.reverse()).range([0, x1.rangeBand()])
+    const leftSeriesAxis = d3.svg.axis().scale(sx1).orient('bottom')
+    leftSeriesAxis.tickValues(rightSeriesAxis.tickValues())
+    svg.selectAll('.series.left')
+    .append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0, ' + (this.size.height - 20) + ')')
+    .call(leftSeriesAxis)
 
     series.selectAll('rect')
     .data(d => d.values)

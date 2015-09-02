38dd08c2b982e76b94f4eb5344c8664ef377f12c
Rune Botten <rbotten@gmail.com>
2015-09-02 16:34:42 +0200

    Add a very ugly tooltip and test the mouse events on some circles at interesting points on the lines in StackedAreaChart

--- components/charts/area-chart/StackedAreaChart.jsx
+++ components/charts/area-chart/StackedAreaChart.jsx
@@ -49,35 +49,36 @@ export default class StackedAreaChart extends React.Component {
     .y0(dataItem => y(dataItem.y0))
     .y1(dataItem => y(dataItem.y0 + dataItem.y))
 
-    data.forEach(dataItem => dataItem.date = parseDate(dataItem.category))
-
-    const stack = d3.layout.stack().values(dataItem => dataItem.values)
-
-    const nest = d3.nest().key(dataItem => dataItem.series).key(dataItem => dataItem.category)
-
-    const preparedData = nest.entries(data).map(s => {
-      return {
-        name: s.key,
-        values: s.values.map(value => {
-          return {date: value.values[0].date, y: value.values[0].value}
-        })
-      }
+    const dates = []
+    const nest = d3.nest().key(dataItem => dataItem.series).key(dataItem => dataItem.category).rollup(leaves => {
+      const leaf = leaves[0]
+      leaf.date = parseDate(leaf.category)
+      dates.push(leaf.date)
+      return leaves
     })
 
-    // Find the biggest Y value for each point on the X axis
-    const maxYvalues = d3.nest().key(item => item.category).entries(data).map(category => {
-      let max = 0
-      category.values.forEach(val => max += val.value)
-      return max
+    const preparedData = nest.entries(data)
+    // Preapre properties for the area() function
+    preparedData.forEach(series => {
+      series.values.forEach(val => {
+        val.date = val.values[0].date
+        val.y = val.values[0].value
+      })
     })
-    const maxY = d3.max(maxYvalues)
-    // Scale the Y axis based on this max value
-    y.domain([0, maxY])
 
     // Scale the X axis by the date range in the data
-    x.domain(d3.extent(data, dataItem => dataItem.date))
+    x.domain(d3.extent(dates))
 
+    // Scale the Y axis based on the max added value across series in a category
+    // FIXME: There must be a better way
+    const seriesValues = d3.nest().key(item => item.category).rollup(leaves => leaves.map(node => node.value)).entries(data)
+    const allValues = seriesValues.map(item => item.values)
+    const summedValues = allValues.map(ary => d3.sum(ary))
+    y.domain([0, d3.max(summedValues)])
+
+    const stack = d3.layout.stack().values(dataItem => dataItem.values)
     const series = stack(preparedData)
+
     svg.selectAll('.area')
     .data(series)
     .enter()
@@ -85,7 +86,7 @@ export default class StackedAreaChart extends React.Component {
     .append('path')
     .attr('class', 'area')
     .attr('d', dataItem => area(dataItem.values))
-    .style('fill', dataItem => color(dataItem.name))
+    .style('fill', dataItem => color(dataItem.key))
     .style('stroke', '#abc')
 
     svg.append('g')
@@ -96,6 +97,36 @@ export default class StackedAreaChart extends React.Component {
     svg.append('g')
     .attr('class', 'axis')
     .call(yAxis)
+
+    const circles = svg.selectAll('.circle')
+    .data(preparedData).enter()
+    .append('g')
+
+    const tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
+    .style('background-color', '#f4f0f0')
+    .style('width', '100px')
+    .style('height', '50px')
+
+    circles.selectAll('circle').data(d => d.values).enter()
+    .append('circle')
+    .attr('cx', dataItem => x(dataItem.date))
+    .attr('cy', dataItem => y(dataItem.y + dataItem.y0))
+    .attr('r', '3')
+    .style('fill', 'black')
+    .on('mouseover', item => {
+      tooltip
+      .style('top', d3.event.pageY - 10 + 'px')
+      .style('left', d3.event.pageX + 10 + 'px')
+
+      const dataItem = item.values[0]
+      tooltip.html('<p>' + dataItem.value + '</p>')
+      tooltip.style('visibility', 'visible')
+    })
+    .on('mouseout', item => tooltip.style('visibility', 'hidden'))
   }
 
   render() {

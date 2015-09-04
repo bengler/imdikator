0922965141ab98c365a2da6dacda61acd187517b
Bjørge Næss <bjoerge@gmail.com>
2015-09-04 15:45:38 +0200

    Conditionally enable the (rather buggy) redux devtools

--- store/index.js
+++ store/index.js
@@ -3,12 +3,15 @@ import {devTools, persistState} from 'redux-devtools'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
-const finalCreateStore = compose(
-  applyMiddleware(thunkMiddleware),
-  // Provides support for DevTools
-  devTools(),
-  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+import config from '../config'
+
+const finalCreateStore = compose.apply(null, [
+    applyMiddleware(thunkMiddleware),
+    // Provides support for DevTools
+    config.reduxDevTools && devTools(),
+    // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+    config.reduxDevTools && persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  ].filter(Boolean) // (I'm so sorry)
 )(createStore)
 
 export default finalCreateStore(rootReducer, {})

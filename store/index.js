49834226e5868e8a0e7dd6943b847146f4242fef
Rune Botten <rbotten@gmail.com>
2015-09-04 15:47:47 +0200

    Add logger middleware for console-logging dispatching

--- store/index.js
+++ store/index.js
@@ -2,9 +2,11 @@ import {compose, createStore, applyMiddleware} from 'redux'
 import {devTools, persistState} from 'redux-devtools'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
+import {logger} from '../middleware'
 
 const finalCreateStore = compose(
   applyMiddleware(thunkMiddleware),
+  applyMiddleware(logger),
   // Provides support for DevTools
   devTools(),
   // Lets you write ?debug_session=<name> in address bar to persist debug sessions

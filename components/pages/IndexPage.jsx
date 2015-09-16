350a398a6c0e0552f8e1dc46598504c548e52f76
Bjørge Næss <bjoerge@gmail.com>
2015-09-16 13:45:20 +0200

    Fix index page

--- components/pages/IndexPage.jsx
+++ components/pages/IndexPage.jsx
@@ -10,14 +10,9 @@ export default class RegionPage extends Component {
       <div>
         IndexPage
         <pre>{JSON.stringify(this.props.route, null, 2)}</pre>
-        <a href="/regions/k0101-Halden/befolkning/befolkninggr2">Go to halden => befolkning => befolkninggr2</a>
-        <p><a href="/regions/foo"> Go to region</a></p>
-        <p><a href="/regions/foo?no=no"> Go to region (w / query)</a></p>
-        <p><a href="/sfdjadskfjskaldj"> Not found - Catch all route!</a></p>
-        <h2>Try some charts</h2>
-        <p><a href="/debug/charts/">CHARTS!</a></p>
-        <p><a href="/doc/">Documentation</a></p>
-        <a onClick={this.handleClick}>No href (just for testing react-a11y)</a>
+        <a href="/regions/k0301-Oslo/befolkning/befolkning_hovedgruppe">/k0301-Oslo/befolkning/befolkning_hovedgruppe</a>
+        <br/>
+        <br/>
       </div>
     )
   }

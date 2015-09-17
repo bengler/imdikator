d7181bfe8165a84dfd8b86b14eb608d27f67c924
Rune Botten <rbotten@gmail.com>
2015-09-17 15:48:02 +0200

    Update stackedArea chart to handle new data format

--- components/charts/area-chart/StackedAreaChart.jsx
+++ components/charts/area-chart/StackedAreaChart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import d3 from 'd3'
 import D3Chart from '../../utils/D3Chart'
 
+import {queryResultNester, nestedQueryResultLabelizer} from '../../../lib/queryResultNester'
+
 /**
  * Only for development
  */
@@ -10,11 +12,14 @@ export default class StackedAreaChart extends React.Component {
     data: React.PropTypes.object
   }
   drawPoints(el, data) {
-    if (!data || !data.hasOwnProperty('data') || !data.hasOwnProperty('unit')) {
+    if (!data) {
       return
     }
 
     const svg = this.svg
+
+    const dimensionLabels = data.dimensions
+    const preparedData = nestedQueryResultLabelizer(queryResultNester(data.rows, dimensionLabels), dimensionLabels)
 
     const parseDate = d3.time.format('%Y').parse
 
@@ -34,43 +39,56 @@ export default class StackedAreaChart extends React.Component {
     .y1(dataItem => y(dataItem.y0 + dataItem.y))
 
     const dates = []
-    const nest = d3.nest().key(dataItem => dataItem.series).key(dataItem => dataItem.category).rollup(leaves => {
-      const leaf = leaves[0]
-      leaf.date = parseDate(String(leaf.category))
-      dates.push(leaf.date)
-      return leaves
+    preparedData.forEach(item => {
+      item.values.forEach(value => {
+        value.date = parseDate(value.key)
+        dates.push(value.date)
+        value.value = parseFloat(value.values[0].tabellvariabel)
+      })
     })
 
-    const preparedData = nest.entries(data.data)
     // Preapre properties for the area() function
     preparedData.forEach(series => {
       series.values.forEach(val => {
-        val.date = val.values[0].date
         val.y = val.values[0].value
-        if (isPercent) {
-          val.y /= 100
-        }
       })
     })
 
     // Scale the X axis by the date range in the data
     x.domain(d3.extent(dates))
 
-    // Scale the y axis based on the data unit
+    svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0,' + this.size.height + ')')
+    .call(xAxis)
+
+
+    // Stack our data
+    const stack = d3.layout.stack().values(dataItem => dataItem.values)
+    const series = stack(preparedData)
+
     if (isPercent) {
+      // Values are percent, use 0,1
       y.domain([0, 1])
       yAxis.tickFormat(d3.format('%'))
     } else {
-      // Scale the Y axis based on the max added value across series in a category
-      const seriesValues = d3.nest().key(item => item.category).rollup(leaves => leaves.map(node => node.value)).entries(data.data)
-      const allValues = seriesValues.map(item => item.values)
-      const summedValues = allValues.map(ary => d3.sum(ary))
-      y.domain([0, d3.max(summedValues)])
-      yAxis.tickFormat(d3.format('d'))
+      // Scale the y axis based on the maximum stacked value
+      let maxStackedValue = 0
+      series.forEach(cat => {
+        cat.values.forEach(item => {
+          const sum = item.y0 + item.y
+          if (sum > maxStackedValue) {
+            maxStackedValue = sum
+          }
+        })
+      })
+      y.domain([0, maxStackedValue])
+      yAxis.tickFormat(d3.format('s'))
     }
 
-    const stack = d3.layout.stack().values(dataItem => dataItem.values)
-    const series = stack(preparedData)
+    svg.append('g')
+    .attr('class', 'axis')
+    .call(yAxis)
 
     svg.selectAll('.area')
     .data(series)
@@ -81,45 +99,6 @@ export default class StackedAreaChart extends React.Component {
     .attr('d', dataItem => area(dataItem.values))
     .style('fill', dataItem => color(dataItem.key))
     .style('stroke', '#abc')
-
-    svg.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(0,' + this.size.height + ')')
-    .call(xAxis)
-
-    svg.append('g')
-    .attr('class', 'axis')
-    .call(yAxis)
-
-    const circles = svg.selectAll('.circle')
-    .data(preparedData).enter()
-    .append('g')
-
-    const tooltip = d3.select('body')
-    .append('div')
-    .style('position', 'absolute')
-    .style('z-index', '10')
-    .style('visibility', 'hidden')
-    .style('background-color', '#f4f0f0')
-    .style('width', '100px')
-    .style('height', '50px')
-
-    circles.selectAll('circle').data(d => d.values).enter()
-    .append('circle')
-    .attr('cx', dataItem => x(dataItem.date))
-    .attr('cy', dataItem => y(dataItem.y + dataItem.y0))
-    .attr('r', '3')
-    .style('fill', 'black')
-    .on('mouseover', item => {
-      tooltip
-      .style('top', d3.event.pageY - 10 + 'px')
-      .style('left', d3.event.pageX + 10 + 'px')
-
-      const dataItem = item.values[0]
-      tooltip.html('<p>' + dataItem.value + '</p>')
-      tooltip.style('visibility', 'visible')
-    })
-    .on('mouseout', item => tooltip.style('visibility', 'hidden'))
   }
 
   render() {

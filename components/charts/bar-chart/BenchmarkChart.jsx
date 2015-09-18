9e47519a0a255736828a5e4650624fbd3f896ae2
Rune Botten <rbotten@gmail.com>
2015-09-18 11:59:49 +0200

    Update BenchmarkChart

--- components/charts/bar-chart/BenchmarkChart.jsx
+++ components/charts/bar-chart/BenchmarkChart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import d3 from 'd3'
 import D3Chart from '../../utils/D3Chart'
 
+import {queryResultNester, nestedQueryResultLabelizer} from '../../../lib/queryResultNester'
+
 export default class BenchmarkChart extends React.Component {
   static propTypes = {
     data: React.PropTypes.object
@@ -9,47 +11,54 @@ export default class BenchmarkChart extends React.Component {
 
   drawPoints(el, data) {
 
-    if (!data || !data.hasOwnProperty('data') || !data.hasOwnProperty('unit')) {
+    if (!data) {
       return
     }
+
+    const dimensionLabels = data.dimensions
+    const preparedData = nestedQueryResultLabelizer(queryResultNester(data.rows, dimensionLabels), dimensionLabels)
 
     const svg = this.svg
     const interimSpacingFactor = 0.025
     const endMarginFactor = 0
     const x = d3.scale.ordinal().rangeRoundBands([0, this.size.width], interimSpacingFactor, endMarginFactor)
-    x.domain(data.data.map(dataItem => dataItem.category))
+    x.domain(preparedData.map(dataItem => dataItem.title))
     const y = d3.scale.linear().range([this.size.height, 0])
 
-    const isPercent = data.unit === 'percent'
+    const isPercent = data.unit === 'prosent'
+    let labelFormat = d3.format('d')
     if (isPercent) {
-      y.domain([0, 100])
+      y.domain([0, 1])
+      labelFormat = d3.format('%')
     } else {
-      y.domain([0, d3.max(data.data, dataItem => dataItem.value)])
+      y.domain([0, preparedData.maxValue])
     }
 
     const labels = []
     // TODO: Move these colors out to CSS?
-    data.data.forEach((dataItem, i) => {
-      if (dataItem.highlight === true) {
+    preparedData.forEach((dataItem, i) => {
+      if (dataItem.values[0].highlight === true) {
         const color = 'red'
         dataItem.color = color
-        const label = String(dataItem.value) + '%'
-        labels.push({x: x(dataItem.category), y: y(dataItem.value), text: label, color: color})
+        labels.push({
+          x: x(dataItem.title),
+          y: y(dataItem.values[0].value),
+          text: labelFormat(dataItem.values[0].value),
+          color: color
+        })
       } else {
         dataItem.color = 'rgb(144, 165, 178)'
       }
     })
 
-    // Draw the background color (TODO: move this out to CSS?)
     svg.append('rect')
-    .attr('class', 'glanceChartBackground')
+    .attr('class', 'benchmarkBackground')
     .attr('width', '100%')
     .attr('height', '100%')
-    .style('fill', 'rgb(223, 235, 241)')
 
     if (isPercent) {
       // Draw the lines per 10% in the background
-      const lines = d3.range(10, 100, 10)
+      const lines = d3.range(y.domain()[0], y.domain()[1], d3.max(y.domain()) / 10)
       svg.selectAll('.line').data(lines).enter()
       .append('line')
       .attr('x1', 0)
@@ -60,13 +69,17 @@ export default class BenchmarkChart extends React.Component {
     }
 
     // Draw the bars
-    svg.selectAll('.bar').data(data.data).enter()
+    svg.selectAll('.bar').data(preparedData).enter()
     .append('rect')
     .attr('class', 'glanceBar')
-    .attr('x', dataItem => x(dataItem.category))
-    .attr('y', dataItem => y(dataItem.value))
+    .attr('x', dataItem => x(dataItem.title))
+    .attr('y', dataItem => {
+      return y(dataItem.values[0].value)
+    })
     .attr('width', dataItem => x.rangeBand())
-    .attr('height', dataItem => this.size.height - y(dataItem.value))
+    .attr('height', dataItem => {
+      return this.size.height - y(dataItem.values[0].value)
+    })
     .style('fill', dataItem => dataItem.color)
 
     // Draw any labels (any datapoint that has highlight === true)
@@ -74,7 +87,9 @@ export default class BenchmarkChart extends React.Component {
     svg.selectAll('.label').data(labels).enter()
     .append('text')
     .attr('dx', dataItem => dataItem.x + x.rangeBand() / 2)
-    .attr('dy', dataItem => dataItem.y - fontSize)
+    .attr('dy', dataItem => {
+      return dataItem.y - fontSize
+    })
     .attr('width', dataItem => x.rangeBand())
     .style('text-anchor', 'middle')
     .style('font-size', String(fontSize) + 'px')

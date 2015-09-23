cac1d348297d1519eee0d9bd35420c1534f52b43
Rune Botten <rbotten@gmail.com>
2015-09-23 13:32:59 +0200

    Add a simple autocomplete search to the front page. Needs design and optimization. Also needs correct routing?

---
+++ components/containers/Search.jsx
@@ -0,0 +1,110 @@
+import React, {Component} from 'react'
+
+import Autocomplete from 'react-autocomplete'
+
+import kommuner from '../../data/kommuner'
+import fylker from '../../data/fylker'
+import naeringsregioner from '../../data/naeringsregioner'
+import bydeler from '../../data/bydeler'
+
+function getRegions() {
+  const regions = []
+
+  bydeler.forEach(item => {
+    item.type = 'bydel'
+    regions.push(item)
+  })
+
+  kommuner.forEach(item => {
+    item.type = 'kommune'
+    regions.push(item)
+  })
+
+  fylker.forEach(item => {
+    item.type = 'fylke'
+    regions.push(item)
+  })
+
+  naeringsregioner.forEach(item => {
+    item.type = 'naeringsregion'
+    regions.push(item)
+  })
+
+  return regions
+}
+
+function matchRegionToTerm(region, value) {
+  return region.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+}
+
+function sortRegions(a, b, value) {
+  return (
+    a.name.toLowerCase().indexOf(value.toLowerCase())
+    > b.name.toLowerCase().indexOf(value.toLowerCase()) ? 1 : -1
+  )
+}
+
+export default class Search extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {regions: getRegions()}
+  }
+
+  // TODO: This has to be completely rewritten
+  navigateToRegion(region) {
+    const id = region.type[0] + region.code
+    window.R.navigate('/steder/' + id + '/befolkning/befolkning_hovedgruppe/latest')
+  }
+
+  renderItems(items) {
+    return items.map((item, index) => {
+      const text = item.props.children
+      let res = item
+      if (index === 0 || items[index - 1].props.children.charAt(0) !== text.charAt(0)) {
+        const style = {
+          background: '#eee',
+          color: '#454545',
+          padding: '2px 6px',
+          fontWeight: 'bold'
+        }
+        res = [<div style={style}>{text.charAt(0)}</div>, item]
+      }
+      return res
+    })
+  }
+
+  render() {
+    const styles = {
+      highlightedItem: {},
+      item: {}
+    }
+
+    return (
+      <Autocomplete
+        items={this.state.regions}
+        getItemValue={item => item.name}
+        shouldItemRender={matchRegionToTerm}
+        onSelect={(value, item) => this.navigateToRegion(item)}
+        sortItems={sortRegions}
+        renderItem={(item, isHighlighted) => (
+          <div
+            style={isHighlighted ? styles.highlightedItem : styles.item}
+            key={item.name + ',' + item.type}
+            id={item.code}
+            >{item.name}</div>
+          )}
+          renderMenu={(items, value, style) => (
+            <div style={{...styles.menu, ...style}}>
+              {this.state.loading ? (
+                <div style={{padding: 6}}>Loading...</div>
+                ) : items.length === 0 ? (
+                <div style={{padding: 6}}>Ingen treff for '{value}'</div>
+                ) : this.renderItems(items)}
+              </div>
+              )}
+            />
+    )
+  }
+
+}
--- components/pages/IndexPage.jsx
+++ components/pages/IndexPage.jsx
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react'
+import Search from '../containers/Search'
 
 export default class RegionPage extends Component {
   static propTypes = {
@@ -11,6 +12,7 @@ export default class RegionPage extends Component {
         IndexPage
         <pre>{JSON.stringify(this.props.route, null, 2)}</pre>
         <a href="/steder/k0301-Oslo/befolkning/befolkning_hovedgruppe/latest">/k0301-Oslo/befolkning/befolkning_hovedgruppe/latest</a>
+        <Search/>
         <br/>
         <br/>
       </div>

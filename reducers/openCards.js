c7fc04e79e07b34d9fd53949930e5e9509056d9d
Bjørge Næss <bjoerge@gmail.com>
2015-09-25 18:04:59 +0200

    Make sure we don't get double entries in state.openCards

--- reducers/openCards.js
+++ reducers/openCards.js
@@ -1,16 +1,11 @@
 import {OPEN_CARD, CLOSE_CARD} from '../actions/cards'
 
-function remove(array, item) {
-  const idx = array.indexOf(item)
-  return [...array.slice(0, idx), ...array.slice(idx + 1)]
-}
-
 export default function cards(state = [], action) {
   switch (action.type) {
     case OPEN_CARD:
-      return remove(state, action.cardName).concat(action.cardName)
+      return state.filter(cardName => cardName !== action.cardName).concat(action.cardName)
     case CLOSE_CARD:
-      return remove(state, action.cardName)
+      return state.filter(cardName => cardName !== action.cardName)
     default:
       return state
   }

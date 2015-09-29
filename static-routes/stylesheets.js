6942a849e6388b0b9bd33ea10d207b6e39ebb48b
Bjørge Næss <bjoerge@gmail.com>
2015-09-29 12:47:19 +0200

    Dynamically import less-files from imdi-styles

--- static-routes/stylesheets.js
+++ static-routes/stylesheets.js
@@ -2,19 +2,22 @@ import less from 'less'
 import fs from 'fs'
 import path from 'path'
 import config from '../config'
+import readdir from 'fs-readdir-recursive'
+
+const imdiStylesRoot = path.dirname(require.resolve('imdi-styles'))
 
 const development = config.env === 'development'
 
+const stylesRelPath = './Styles/global'
+
 export default {
   '/build/stylesheets/main.css'() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(require.resolve('../stylesheets/bundles/main.less'), 'utf-8', (err, buffer) => {
-        if (err) {
-          return reject(err)
-        }
-        resolve(buffer)
-      })
-    })
+
+    const importStatements = readdir(path.join(imdiStylesRoot, stylesRelPath))
+      .map(file => `@import "${path.join('imdi-styles', stylesRelPath, file)}";`)
+    .concat(`@import "./stylesheets/bundles/main.less";`)
+
+    return Promise.resolve(importStatements.join('\n'))
       .then(buffer => {
         return less.render(buffer, {
           outFile: '/stylesheets/main.css',

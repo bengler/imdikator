995b971bf065f3218de697448d8b8f209fdd6d0c
Thomas Drevon <thomas@bengler.no>
2015-09-30 10:56:32 +0200

    Make the links to child regions work

--- components/elements/RegionChildrenList.jsx
+++ components/elements/RegionChildrenList.jsx
@@ -1,8 +1,14 @@
 import React, {Component, PropTypes} from 'react'
+import {prefixify} from '../../lib/regionUtil'
 
 export default class RegionChildrenList extends Component {
+
   static propTypes = {
     children: PropTypes.array
+  }
+
+  static contextTypes = {
+    linkTo: PropTypes.func
   }
 
   render() {
@@ -10,7 +16,11 @@ export default class RegionChildrenList extends Component {
       <nav role="navigation" className="navigation">
         <ul className="t-no-list-styles">
           {this.props.children.map(region => {
-            return <li key={region.code} className="col--third col--flow col--right-padding"><a className="navigation__link" href="#">{region.name}</a></li>
+            return (
+            <li key={region.code} className="col--third col--flow col--right-padding">
+              <a className="navigation__link" href={this.context.linkTo('/steder/:region', {region: prefixify(region)})}>{region.name}</a>
+            </li>
+            )
           })}
         </ul>
       </nav>

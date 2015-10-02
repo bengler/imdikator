3a70e7937f953ce73432312f959e35fbece231c6
Thomas Drevon <thomas@bengler.no>
2015-10-02 11:16:46 +0200

    Call up the table header reducer when we receive headers

--- actions/chartFodder.js
+++ actions/chartFodder.js
@@ -5,6 +5,8 @@ import {queryResultPresenter} from '../lib/queryResultPresenter'
 
 export const REQUEST_CHART_DATA = 'REQUEST_CHART_DATA'
 export const RECEIVE_CHART_DATA = 'RECEIVE_CHART_DATA'
+export const RECEIVE_TABLE_HEADERS = 'RECEIVE_TABLE_HEADERS'
+
 
 export function loadChartData(region, userQuery, chartKind) {
   return (dispatch, getState) => {
@@ -14,6 +16,11 @@ export function loadChartData(region, userQuery, chartKind) {
     })
 
     apiClient.getHeaderGroups(userQuery.tableName).then(headerGroups => {
+      dispatch({
+        type: RECEIVE_TABLE_HEADERS,
+        headers: headerGroups,
+        tableName: userQuery.tableName
+      })
       const newQuery = Object.assign({}, userQuery, {region: prefixify(region)})
       const resolvedQuery = resolveQuery(region, newQuery, headerGroups)
 

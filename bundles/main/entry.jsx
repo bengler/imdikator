31a2b8dcbe9273ee95667d7ce987e746801ab155
Bjørge Næss <bjoerge@gmail.com>
2015-10-02 10:54:20 +0200

    Make it possible to hide redux devtools

--- bundles/main/entry.jsx
+++ bundles/main/entry.jsx
@@ -23,29 +23,57 @@ if (containers.length !== 1) {
 
 router.navigate(document.location.pathname)
 
-ReactDOM.render(
-  // The child must be wrapped in a function
-  // to work around an issue in React 0.13.
-  <div>
-    <Provider store={app}>
-      <App router={router}/>
-    </Provider>
+function isDevToolsVisible() {
+  return window.localStorage.getItem('reduxDevToolsEnabled') !== 'false'
+}
+function toggleDevToolsVisibility() {
+  window.localStorage.setItem('reduxDevToolsEnabled', isDevToolsVisible() ? 'false' : 'true')
+  render()
+}
 
-    {config.reduxDevTools && (
-      <DebugPanel style={{backgroundColor: '#444'}} top right bottom>
-        <div style={{padding: 4}}>
-          Pro tip: Turn off redux devtools with:
-          <input type="text" readOnly
-                 onFocus={e => (target => setTimeout(() => target.select(), 0))(e.target)}
-                 style={{margin: 0, lineHeight: 1, color: 'inherit', backgroundColor: 'inherit', padding: 2, border: '1px solid #aaa'}}
-                 value="REDUX_DEVTOOLS=0 npm start"/>
-        </div>
-        <DevTools store={app} monitor={LogMonitor} />
-      </DebugPanel>
-    )}
-  </div>,
-  containers[0]
-)
+function ReduxDevToolsPanel() {
+  if (!config.reduxDevTools) {
+    return null
+  }
+  const devToolsIsVisible = isDevToolsVisible()
+
+  if (!devToolsIsVisible) {
+    return <button style={{fontSize: 10, width: 150, position: 'absolute', top: 0, right: 0}} onClick={toggleDevToolsVisibility}>Show Redux Devtools</button>
+
+  }
+  return (
+    <DebugPanel style={{backgroundColor: '#444'}} top right bottom>
+      <div style={{padding: 4}}>
+        Pro tip: Turn off redux devtools with:
+        <input type="text" readOnly
+               onFocus={e => (target => setTimeout(() => target.select(), 0))(e.target)}
+               style={{margin: 0, lineHeight: 1, color: 'inherit', backgroundColor: 'inherit', padding: 2, border: '1px solid #aaa'}}
+               value="REDUX_DEVTOOLS=0 npm start"/>
+          <div style={{marginTop: 5}}>
+            You can also <a href="#" style={{color: 'lightblue', padding: 2}} onClick={toggleDevToolsVisibility}>
+            Hide this panel &raquo;
+          </a></div>
+      </div>
+      <DevTools store={app} monitor={LogMonitor}/>
+    </DebugPanel>
+  )
+}
+
+function render() {
+  ReactDOM.render(
+    // The child must be wrapped in a function
+    // to work around an issue in React 0.13.
+    <div>
+      <Provider store={app}>
+        <App router={router}/>
+      </Provider>
+      <ReduxDevToolsPanel/>
+    </div>,
+    containers[0]
+  )
+}
+
+render()
 
 setTimeout(() => {
   // Need to bind the global click listener *after* react has mounted and bound its global listener to document

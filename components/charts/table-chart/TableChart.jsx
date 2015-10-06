8ec2ebb8e8679199ef4f017f0b7fd61c9f0862e1
Rune Botten <rbotten@gmail.com>
2015-10-06 12:41:57 +0200

    Adds support for CSV in TableChart

--- components/charts/table-chart/TableChart.jsx
+++ components/charts/table-chart/TableChart.jsx
@@ -27,46 +27,56 @@ export default class TableChart extends React.Component {
 
     // Generate TSV (for downloading, and we use this to draw the table)
     let tsv = ''
+    const separator = ','
+
     let parsedData = []
     if (data.dimensions.length > 0) {
       // See 04-befolkning_alder-fylke-2014.csv
       const nester = d3.nest().key(item => item[regionKey]).sortKeys(d3.ascending).key(item => data.dimensions.join(','))
       const xx = nester.entries(data.rows)
       // Headers
-      tsv += regionKey + '\tNavn'
+      tsv += regionKey + separator + 'Navn'
       data.dimensions.forEach((dimension, idx) => {
-        tsv += '\t'
+        tsv += separator
         if (idx > 0) {
-          tsv += '\t'
+          tsv += separator
         }
         xx[0].values[0].values.forEach(row => {
-          tsv += dimension + '.' + row[dimension] + '\t'
+          tsv += dimension + '.' + row[dimension] + separator
         })
         tsv += '\n'
       })
 
       xx.forEach(region => {
-        tsv += region.key + '\t' + dimensionLabelTitle(regionKey, region.key) + '\t'
+        tsv += region.key + separator + dimensionLabelTitle(regionKey, region.key) + separator
         region.values[0].values.forEach(row => {
-          tsv += row.tabellvariabel + '\t'
+          tsv += row.tabellvariabel + separator
         })
         tsv += '\n'
       })
     } else {
       // Flat render, all keys as table headers, all values as rows
       const dimensions = Object.keys(data.rows[0])
-      tsv += dimensions.join('\t')
+      tsv += dimensions.join(separator)
       tsv += '\n'
       data.rows.forEach(row => {
         dimensions.forEach(dim => {
           tsv += row[dim]
-          tsv += '\t'
+          tsv += separator
         })
         tsv += '\n'
       })
     }
 
-    parsedData = d3.tsv.parseRows(tsv)
+    let parser = null
+    switch (separator) {
+      case '\t':
+        parser = d3.tsv
+        break
+      default:
+        parser = d3.csv
+    }
+    parsedData = parser.parseRows(tsv)
 
     const rows = tableBody.selectAll('tr')
     .data(parsedData)

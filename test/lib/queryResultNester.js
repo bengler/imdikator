71aba31ee3ac854b4ab60bf5d85b6a0457fd9bc3
Rune Botten <rbotten@gmail.com>
2015-10-12 13:43:03 +0200

    Change labelling of regions to support one common property key and prefixed identifiers

--- test/lib/queryResultNester.js
+++ test/lib/queryResultNester.js
@@ -8,8 +8,8 @@ describe('queryResultNester', () => {
   it('groups data according to displayNesting down to categories and series', () => {
 
     const nested = queryResultNester([
-      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '0', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '1', innvkat5: 'alle', fylkeNr: '01'},
+      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '0', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '1', innvkat5: 'alle', region: 'F01'},
     ], ['aar', 'innvkat5', 'kjonn'])
 
     //console.log(util.inspect(nested, {showHidden: false, depth: null}))
@@ -23,39 +23,39 @@ describe('queryResultNester', () => {
 
   it('saves a property maxValue with the biggest value from leaf nodes', () => {
     let nested = queryResultNester([
-      {aar: '2014', tabellvariabel: '119', enhet: 'personer', kjonn: '0', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '1', innvkat5: 'alle', fylkeNr: '01'},
+      {aar: '2014', tabellvariabel: '119', enhet: 'personer', kjonn: '0', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '29', enhet: 'personer', kjonn: '1', innvkat5: 'alle', region: 'F01'},
     ], ['aar', 'innvkat5', 'kjonn'])
 
     assert.equal(nested.maxValue, 119)
 
     nested = queryResultNester([
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', fylkeNr: '01'},
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', region: 'F01'},
     ], ['aar', 'innvkat5'])
 
     assert.equal(nested.maxValue, 200)
 
     nested = queryResultNester([
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', fylkeNr: '02'},
-      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', fylkeNr: '02'},
-    ], ['aar', 'fylkeNr'])
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', region: 'F01'},
+      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', region: 'F02'},
+      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', region: 'F02'},
+    ], ['aar', 'region'])
 
     assert.equal(nested.maxValue, 1000)
 
     nested = queryResultNester([
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', fylkeNr: '01'},
-      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', fylkeNr: '02'},
-      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', fylkeNr: '02'},
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '0', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '100', enhet: 'personer', kjonn: '1', innvkat5: 'alle', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', region: 'F01'},
+      {aar: '2014', tabellvariabel: '50', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', region: 'F01'},
+      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '0', innvkat5: 'ingen', region: 'F02'},
+      {aar: '2014', tabellvariabel: '500', enhet: 'personer', kjonn: '1', innvkat5: 'ingen', region: 'F02'},
     ], ['aar', 'innvkat5'])
 
     assert.equal(nested.maxValue, 50 + 50 + 500 + 500)
@@ -77,37 +77,37 @@ describe('nestedQueryResultLabelizer', () => {
   })
 
   it('knows about cities', () => {
-    const nested = queryResultNester([{kommuneNr: '1505'}], ['kommuneNr'])
-    const labelized = nestedQueryResultLabelizer(nested, ['kommuneNr'])
+    const nested = queryResultNester([{region: 'K1505'}], ['region'])
+    const labelized = nestedQueryResultLabelizer(nested, ['region'])
     assert.equal(labelized[0].title, 'Kristiansund')
   })
 
   it('knows about state names', () => {
-    const nested = queryResultNester([{fylkeNr: '03'}], ['fylkeNr'])
-    const labelized = nestedQueryResultLabelizer(nested, ['fylkeNr'])
+    const nested = queryResultNester([{region: 'F03'}], ['region'])
+    const labelized = nestedQueryResultLabelizer(nested, ['region'])
     assert.equal(labelized[0].title, 'Oslo')
   })
 
   it('knows about boroughs', () => {
     const nested = queryResultNester([
-      {bydelNr: '030102'}
-    ], ['bydelNr'])
+      {region: 'B030102'}
+    ], ['region'])
 
     const labelized = nestedQueryResultLabelizer(
       nested
-    , ['bydelNr'])
+    , ['region'])
 
     assert.equal(labelized[0].title, 'Grünerløkka')
   })
 
   it('it labels business areas', () => {
     const nested = queryResultNester([
-      {naringsregionNr: '03'}
-    ], ['naringsregionNr'])
+      {region: 'N03'}
+    ], ['region'])
 
     const labelized = nestedQueryResultLabelizer(
       nested
-    , ['naringsregionNr'])
+    , ['region'])
 
     assert.equal(labelized[0].title, 'Mosseregionen')
   })

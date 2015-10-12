60f80d1ab153f27e8d20c65d2ecfcffaa821234b
Bjørge Næss <bjoerge@gmail.com>
2015-10-12 17:05:38 +0200

    Add missing file

---
+++ components/containers/RegionQuickSwitch.jsx
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {prefixifyRegion} from '../../lib/regionUtil'
+import RegionSearch from './RegionSearch'
+
+class RegionQuickSwitch extends Component {
+  static contextTypes = {
+    linkTo: PropTypes.func,
+    goTo: PropTypes.func
+  }
+
+  handleSelectRegion(region) {
+    this.context.goTo('/steder/:region', {region: prefixifyRegion(region)})
+  }
+
+  render() {
+    return (
+      <div className="search search--autocomplete">
+        <label className="t-margin-top--large">Se tall og statistikk fra et annet sted
+          <RegionSearch onSelect={this.handleSelectRegion.bind(this)}/>
+        </label>
+      </div>
+    )
+  }
+}
+
+
+// Wrap the component to inject dispatch and state into it
+export default connect()(RegionQuickSwitch)

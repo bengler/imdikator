083f87719f42c36f10fe812f6c0628409ba2a580
Joakim Bording <joakim@netliferesearch.com>
2015-10-13 00:39:41 +0200

    Added dummy function buttons

--- components/containers/Card.jsx
+++ components/containers/Card.jsx
@@ -229,8 +229,19 @@ class Card extends Component {
           filters={this.getFilterState()}
           onChange={this.handleFilterChange.bind(this)}
           />
-        <ChartComponent data={this.props.data}/>
-        <small>{tableDescription}</small>
+        <div className="graph">  
+          <ChartComponent data={this.props.data}/>
+        </div>
+        <div className="graph__annotations">{tableDescription}</div>      
+        <div className="graph__functions">
+		  <button type="button" className="button button--secondary button--small"><i className="icon__export"></i> Lenke til grafen</button>				
+		  <button type="button" className="button button--secondary button--small"><i className="icon__download"></i> Last ned</button>
+	    </div>
+	    <div className="graph__about">										
+		  <div className="toggle toggle--light t-no-margin">
+	        <a href="#" className="toggle__button"><span className="toggle__caption--contracted"> Veiledning og kilder</span><span className="toggle__caption--expanded">Skjul veiledning og kilder</span> <i className="icon__arrow-down toggle__icon"></i></a>
+		  </div>        
+        </div>
       </div>
     )
   }

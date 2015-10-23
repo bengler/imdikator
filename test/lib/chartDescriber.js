685cb3146fe24bc6cbedbfa305e1eff491847039
Thomas Drevon <thomas@bengler.no>
2015-10-23 10:59:54 +0200

    Get rid of unused option

--- test/lib/chartDescriber.js
+++ test/lib/chartDescriber.js
@@ -104,7 +104,7 @@ describe('chartDescriber', () => {
 
     opts.regions = ['Sandefjord']
     opts.comparisonType = 'kommuner'
-    expected = 'Figuren viser flyktninger i 2013 fra Sandefjord sammenlignet med lignende kommuner.'
+    expected = 'Figuren viser flyktninger i 2013 fra Sandefjord.'
     assert.equal(describeChart(opts), expected)
   })
 
@@ -118,20 +118,6 @@ describe('chartDescriber', () => {
       regions: ['Sandefjord', 'Drøbak', 'Larvik', 'Bø i Telemark']
     }
     const expected = 'Figuren viser flyktninger med aldersfordeling avgrenset til 0-3 år, kjønnsfordeling avgrenset til kvinner og bakgrunn avgrenset til innvandrere fordelt på flyktninger og befolkningsgrupper i perioden 2012 til 2014 fra Sandefjord, Drøbak, Larvik og Bø i Telemark.'
-    assert.equal(describeChart(opts), expected)
-  })
-
-
-  it('describes everything at once, with comaparisionType', () => {
-    const opts = {
-      showing: 'flyktninger',
-      bounds: ['aldersfordeling avgrenset til 0-3 år', 'kjønnsfordeling avgrenset til kvinner', 'bakgrunn avgrenset til innvandrere'],
-      groupedBy: ['flyktninger', 'befolkningsgrupper'],
-      timePeriod: ['2012', '2014'],
-      regions: ['Sandefjord'],
-      comparisonType: 'kommuner'
-    }
-    const expected = 'Figuren viser flyktninger med aldersfordeling avgrenset til 0-3 år, kjønnsfordeling avgrenset til kvinner og bakgrunn avgrenset til innvandrere fordelt på flyktninger og befolkningsgrupper i perioden 2012 til 2014 fra Sandefjord sammenlignet med lignende kommuner.'
     assert.equal(describeChart(opts), expected)
   })
 

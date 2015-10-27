0d459e631ab9a419df5a0abd6210546c86dc5352
Rune Botten <rbotten@gmail.com>
2015-10-27 10:13:47 +0100

    Add new tests for unitFormatter

--- test/lib/unitFormatter.js
+++ test/lib/unitFormatter.js
@@ -18,23 +18,35 @@ describe('unitFormatter', () => {
     assert.equal(formatters.axisFormat(1999), expected)
   })
 
+  it('shortens billions to mrd', () => {
+    const formatters = unitFormatter('kroner')
+    assert.equal(formatters.format(9223716271), '9.2 mrd kr')
+  })
+
+  it('shortens millions to mill', () => {
+    const formatters = unitFormatter('kroner')
+    assert.equal(formatters.format(9283716), '9.3 mill kr')
+  })
+
   it('formats per mil with ‰ character and 2 decimals', () => {
     const formatters = unitFormatter('promille')
-    assert.equal(formatters.format(1.9), '1.90 ‰')
-    assert.equal(formatters.format(19), '19.00 ‰')
-    assert.equal(formatters.format(0.7512312), '0.75 ‰')
+    assert.equal(formatters.format(1.9), '1.9 ‰')
+    assert.equal(formatters.format(19), '19.0 ‰')
+    // Rounds
+    assert.equal(formatters.format(0.7552312), '0.8 ‰')
 
-    assert.equal(formatters.axisFormat(1.9), '1.90 ‰')
-    assert.equal(formatters.axisFormat(19), '19.00 ‰')
-    assert.equal(formatters.axisFormat(0.7512312), '0.75 ‰')
+    assert.equal(formatters.axisFormat(1.9), '1.9 ‰')
+    assert.equal(formatters.axisFormat(19), '19.0 ‰')
+    // Rounds
+    assert.equal(formatters.axisFormat(0.7212312), '0.7 ‰')
   })
 
   it('formats per cent using 2 decimals and adding % character', () => {
     const formatters = unitFormatter('prosent')
-    assert.equal(formatters.format(190), '190.00 %')
-    assert.equal(formatters.format(29), '29.00 %')
-    assert.equal(formatters.format(29.29), '29.29 %')
-    assert.equal(formatters.format(0.9), '0.90 %')
+    assert.equal(formatters.format(190), '190.0 %')
+    assert.equal(formatters.format(29), '29.0 %')
+    assert.equal(formatters.format(29.29), '29.3 %')
+    assert.equal(formatters.format(0.9), '0.9 %')
   })
 
   it('formats per cent on axis with no decimals', () => {

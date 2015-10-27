5d2ad3cdbe41b620207905ca9ccd9b601ad30671
Thomas Drevon <thomas@bengler.no>
2015-10-27 11:02:39 +0100

    Wire up the facts page

--- bundles/main/routes.js
+++ bundles/main/routes.js
@@ -4,6 +4,7 @@ import IndexPage from '../../components/pages/IndexPage'
 import NotFoundPage from '../../components/pages/NotFoundPage'
 import ChartsPage from '../../components/pages/ChartsPage'
 import CardsPage from '../../components/pages/CardsPage'
+import FactsPage from '../../components/pages/FactsPage'
 import TestRegionSelect from '../../components/testbed/TestRegionSelect'
 import RenderTestPage from '../../components/testbed/RenderTestPage'
 
@@ -15,6 +16,7 @@ export default {
   '/debug/region-select': TestRegionSelect,
   '/steder/:region': RegionPage,
   '/steder/:region/ligner': SimilarRegionsPage,
+  '/steder/:region/fakta': FactsPage,
   '/steder/:region/:pageName': CardsPage,
   '/steder/:region/:pageName/:cardName': CardsPage,
   '/steder/:region/:pageName/:cardName/:tabName': CardsPage,

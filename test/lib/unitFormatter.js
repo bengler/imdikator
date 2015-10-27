02ced6f028b7b25b26b86069de730dc6fd7b6d2b
Rune Botten <rbotten@gmail.com>
2015-10-27 11:11:26 +0100

    &nbsp; all the formatters

--- test/lib/unitFormatter.js
+++ test/lib/unitFormatter.js
@@ -10,10 +10,11 @@ describe('unitFormatter', () => {
     assert.typeOf(formatters.axisFormat, 'function')
   })
 
-  it('formats money with toLocaleString()', () => {
+  it('formats money with toLocaleString() with spaces replaced with nbsp', () => {
     const formatters = unitFormatter('kroner')
 
-    const expected = `${parseFloat(1999).toLocaleString()} kr`
+    // '1999 kr'
+    const expected = `${parseFloat(1999).toLocaleString()}\u00A0kr`
     assert.equal(formatters.format(1999), expected)
     assert.equal(formatters.axisFormat(1999), expected)
   })
@@ -30,29 +31,29 @@ describe('unitFormatter', () => {
 
   it('formats per mil with ‰ character and 2 decimals', () => {
     const formatters = unitFormatter('promille')
-    assert.equal(formatters.format(1.9), '1.9 ‰')
-    assert.equal(formatters.format(19), '19.0 ‰')
+    assert.equal(formatters.format(1.9), '1.9\u00A0‰')
+    assert.equal(formatters.format(19), '19.0\u00A0‰')
     // Rounds
-    assert.equal(formatters.format(0.7552312), '0.8 ‰')
+    assert.equal(formatters.format(0.7552312), '0.8\u00A0‰')
 
-    assert.equal(formatters.axisFormat(1.9), '1.9 ‰')
-    assert.equal(formatters.axisFormat(19), '19.0 ‰')
+    assert.equal(formatters.axisFormat(1.9), '1.9\u00A0‰')
+    assert.equal(formatters.axisFormat(19), '19.0\u00A0‰')
     // Rounds
-    assert.equal(formatters.axisFormat(0.7212312), '0.7 ‰')
+    assert.equal(formatters.axisFormat(0.7212312), '0.7\u00A0‰')
   })
 
   it('formats per cent using 2 decimals and adding % character', () => {
     const formatters = unitFormatter('prosent')
-    assert.equal(formatters.format(190), '190.0 %')
-    assert.equal(formatters.format(29), '29.0 %')
-    assert.equal(formatters.format(29.29), '29.3 %')
-    assert.equal(formatters.format(0.9), '0.9 %')
+    assert.equal(formatters.format(190), '190.0\u00A0%')
+    assert.equal(formatters.format(29), '29.0\u00A0%')
+    assert.equal(formatters.format(29.29), '29.3\u00A0%')
+    assert.equal(formatters.format(0.9), '0.9\u00A0%')
   })
 
   it('formats per cent on axis with no decimals', () => {
     const formatters = unitFormatter('prosent')
-    assert.equal(formatters.axisFormat(19), '19 %')
-    assert.equal(formatters.axisFormat(0.29), '0 %')
+    assert.equal(formatters.axisFormat(19), '19\u00A0%')
+    assert.equal(formatters.axisFormat(0.29), '0\u00A0%')
   })
 
   it('formats unknown units with toLocaleString()', () => {

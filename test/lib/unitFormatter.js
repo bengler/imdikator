e981a5785f81edcee1b61bab5aa87432c10aaefd
Rune Botten <rbotten@gmail.com>
2015-10-27 12:11:10 +0100

    Get the formatters in order. Comma separated thousands grouped with spaces. Removing any ,0.

--- test/lib/unitFormatter.js
+++ test/lib/unitFormatter.js
@@ -14,40 +14,42 @@ describe('unitFormatter', () => {
     const formatters = unitFormatter('kroner')
 
     // '1999 kr'
-    const expected = `${parseFloat(1999).toLocaleString()}\u00A0kr`
+    const expected = '1\u00A0999\u00A0kr'
     assert.equal(formatters.format(1999), expected)
     assert.equal(formatters.axisFormat(1999), expected)
   })
 
   it('shortens billions to mrd', () => {
     const formatters = unitFormatter('kroner')
-    assert.equal(formatters.format(9223716271), '9.2 mrd kr')
+    assert.equal(formatters.format(9223716271), '9,2\u00A0mrd\u00A0kr')
+    assert.equal(formatters.format(1000000000), '1\u00A0mrd\u00A0kr')
   })
 
   it('shortens millions to mill', () => {
     const formatters = unitFormatter('kroner')
-    assert.equal(formatters.format(9283716), '9.3 mill kr')
+    assert.equal(formatters.format(9283716), '9,3\u00A0mill\u00A0kr')
+    assert.equal(formatters.format(1000000), '1\u00A0mill\u00A0kr')
   })
 
   it('formats per mil with ‰ character and 2 decimals', () => {
     const formatters = unitFormatter('promille')
-    assert.equal(formatters.format(1.9), '1.9\u00A0‰')
-    assert.equal(formatters.format(19), '19.0\u00A0‰')
+    assert.equal(formatters.format(1.9), '1,9\u00A0‰')
+    assert.equal(formatters.format(19), '19\u00A0‰')
     // Rounds
-    assert.equal(formatters.format(0.7552312), '0.8\u00A0‰')
+    assert.equal(formatters.format(0.7552312), '0,8\u00A0‰')
 
-    assert.equal(formatters.axisFormat(1.9), '1.9\u00A0‰')
-    assert.equal(formatters.axisFormat(19), '19.0\u00A0‰')
+    assert.equal(formatters.axisFormat(1.9), '1,9\u00A0‰')
+    assert.equal(formatters.axisFormat(19), '19\u00A0‰')
     // Rounds
-    assert.equal(formatters.axisFormat(0.7212312), '0.7\u00A0‰')
+    assert.equal(formatters.axisFormat(0.7212312), '0,7\u00A0‰')
   })
 
   it('formats per cent using 2 decimals and adding % character', () => {
     const formatters = unitFormatter('prosent')
-    assert.equal(formatters.format(190), '190.0\u00A0%')
-    assert.equal(formatters.format(29), '29.0\u00A0%')
-    assert.equal(formatters.format(29.29), '29.3\u00A0%')
-    assert.equal(formatters.format(0.9), '0.9\u00A0%')
+    assert.equal(formatters.format(190), '190\u00A0%')
+    assert.equal(formatters.format(29), '29\u00A0%')
+    assert.equal(formatters.format(29.29), '29,3\u00A0%')
+    assert.equal(formatters.format(0.9), '0,9\u00A0%')
   })
 
   it('formats per cent on axis with no decimals', () => {
@@ -56,16 +58,16 @@ describe('unitFormatter', () => {
     assert.equal(formatters.axisFormat(0.29), '0\u00A0%')
   })
 
-  it('formats unknown units with toLocaleString()', () => {
+  it('formats unknown units like this', () => {
     const formatters = unitFormatter('something')
-    assert.equal(formatters.format(1.9), `${parseFloat(1.9).toLocaleString()}`)
-    assert.equal(formatters.format(19), `${parseFloat(19).toLocaleString()}`)
-    assert.equal(formatters.format(1.292929), `${parseFloat(1.292929).toLocaleString()}`)
-    assert.equal(formatters.format(292929), `${parseFloat(292929).toLocaleString()}`)
+    assert.equal(formatters.format(1.9), '1,9')
+    assert.equal(formatters.format(19), '19')
+    assert.equal(formatters.format(1.292929), '1,3')
+    assert.equal(formatters.format(292929), '292\u00A0929')
 
-    assert.equal(formatters.axisFormat(1.9), `${parseFloat(1.9).toLocaleString()}`)
-    assert.equal(formatters.axisFormat(19), `${parseFloat(19).toLocaleString()}`)
-    assert.equal(formatters.axisFormat(1.292929), `${parseFloat(1.292929).toLocaleString()}`)
-    assert.equal(formatters.axisFormat(292929), `${parseFloat(292929).toLocaleString()}`)
+    assert.equal(formatters.axisFormat(1.9), '1,9')
+    assert.equal(formatters.axisFormat(19), '19')
+    assert.equal(formatters.axisFormat(1.292929), '1,3')
+    assert.equal(formatters.axisFormat(292929), '292\u00A0929')
   })
 })

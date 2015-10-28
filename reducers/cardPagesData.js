08c17545f51da627389c91921ebe1ba51016da31
Thomas Drevon <thomas@bengler.no>
2015-10-28 13:55:32 +0100

    Add new reducer which aggregates all card page data

---
+++ reducers/cardPagesData.js
@@ -0,0 +1,13 @@
+import {RECEIVE_CARD_PAGE_DATA} from '../actions/actions'
+
+export default function cardPagesData(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_CARD_PAGE_DATA:
+      const {cardPageData} = action
+      return Object.assign({}, state, {
+        [cardPageData.name]: cardPageData
+      })
+    default:
+      return state
+  }
+}

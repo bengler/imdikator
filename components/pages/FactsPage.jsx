17a8dc050e0bedf01ad87acfecb56311019d2526
Thomas Drevon <thomas@bengler.no>
2015-10-28 14:04:41 +0100

    Finally a FactPage which works

--- components/pages/FactsPage.jsx
+++ components/pages/FactsPage.jsx
@@ -1,35 +1,43 @@
 import React, {Component, PropTypes} from 'react'
-import Card from '../containers/Card'
 import {connect} from 'react-redux'
-import {loadCardPage} from '../../actions/cardPages'
-import {openCard, closeCard} from '../../actions/cards'
-import CardPageButtons from '../containers/CardPageButtons'
-import RegionQuickSwitch from '../containers/RegionQuickSwitch'
-import RegionInfo from '../elements/RegionInfo'
-import cardPagesConfig from '../../data/cardPages'
+import Card from '../containers/Card'
+import {loadCardPageData, loadCardPages} from '../../actions/cardPages'
+import {openCard} from '../../actions/cards'
 import {_t} from '../../lib/translate'
 
+const cardsConfig = {}
 
 function loadData(props) {
-  const {route, dispatch} = props
-  const prefixedRegionCode = route.params.region.split('-')[0].toUpperCase()
+  const {route, dispatch, cardPagesConfig} = props
 
-  cardPagesConfig.forEach(page => {
-    dispatch(loadCardPage({pageName: page.name, regionCode: prefixedRegionCode, activeTabName: 'latest'}))
-    page.cards.forEach(card => {
-      dispatch(openCard(card.name))
+  const missingConfig = cardPagesConfig.length == 0
+
+  if (missingConfig) {
+    dispatch(loadCardPages())
+  } else {
+    const prefixedRegionCode = route.params.region.split('-')[0].toUpperCase()
+    cardPagesConfig.forEach(page => {
+      page.cards.forEach(card => {
+        cardsConfig[card.name] = card
+        dispatch(loadCardPageData({
+          pageName: page.name,
+          activeCardName: card.name,
+          regionCode: prefixedRegionCode,
+          activeTabName: 'latest'
+        }))
+        dispatch(openCard(card.name))
+      })
     })
-  })
+  }
 }
 
 class FactsPage extends Component {
   static propTypes = {
     route: PropTypes.object,
     dispatch: PropTypes.func,
-    cardPages: PropTypes.array,
     region: PropTypes.object,
-    allRegions: PropTypes.array,
-    cards: PropTypes.array
+    cardPagesConfig: PropTypes.array,
+    cardPagesData: PropTypes.object
   }
 
   static contextTypes = {
@@ -38,26 +46,21 @@ class FactsPage extends Component {
   }
 
   componentWillMount() {
-    console.log('componentWillMount')
     loadData(this.props)
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.route.url !== this.props.route.url) {
+    if (nextProps.cardPagesConfig.length > 0 && this.props.cardPagesConfig.length == 0) {
       loadData(nextProps)
     }
   }
 
   render() {
-    const {pageConfig, region, openCards, allRegions, cards} = this.props
-    return (
-      <pre>{JSON.stringify(cards, 0, 2)}</pre>
-    )
+    const {region, cardPagesConfig, cardPagesData} = this.props
 
-    if (!pageConfig || !region) {
+    if (!region || !cardPagesConfig || !cardPagesData) {
       return <div>Loading...</div>
     }
-
 
     return (
       <div>
@@ -65,7 +68,6 @@ class FactsPage extends Component {
           <div className="wrapper">
             <div className="row">
               <div className="col--main-wide">
-
                 <header>
                   <h1>Integreringen i {region.name} {_t(region.type)}</h1>
                   <p className="ingress">Tall og statistikk over integreringen i {_t('the-' + region.type)}</p>
@@ -79,23 +81,31 @@ class FactsPage extends Component {
           <div className="wrapper">
             <div className="row">
               <div className="col--main">
-
-                <h2 className="feature__section-title">{pageConfig.title} i {region.name}</h2>
-                <ul className="t-no-list-styles">
-                  {pageConfig.cards.map(card => {
-                    const isOpen = openCards.includes(card.name)
-                    return (
-                      <li key={card.name}>
-                        <section className="toggle-list">
-                          <h3 className="toggle-list__button-title">{card.title}
-                            <i className="icon__arrow-down toggle-list__button-icon"/>
-                          </h3>
-                          {isOpen && <Card card={card}/>}
-                        </section>
-                      </li>
-                    )
-                  })}
-                </ul>
+                {cardPagesConfig.map(cardPageConfig => {
+                  return (
+                    <div key={cardPageConfig.name}>
+                      <h2 className="feature__section-title">{cardPageConfig.title} i {region.name}</h2>
+                      <ul className="t-no-list-styles">
+                        {cardPageConfig.cards.map(cardConfig => {
+                          if (!cardPagesData[cardPageConfig.name]) {
+                            return null
+                          }
+                          const card = cardPagesData[cardPageConfig.name].cards.find(aCard => aCard.name == cardConfig.name)
+                          return (
+                            <li key={card.name}>
+                              <section className="toggle-list">
+                                <h3 className="toggle-list__button-title">{card.title}
+                                  <i className="icon__arrow-down toggle-list__button-icon"/>
+                                </h3>
+                                <Card card={card} pageName={cardPageConfig.name}/>
+                              </section>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </div>
+                  )
+                })}
               </div>
             </div>
           </div>
@@ -108,9 +118,8 @@ class FactsPage extends Component {
 function mapStateToProps(state) {
   return {
     region: state.region,
-    allRegions: state.allRegions,
-    cardPages: state.cardPages,
-    cards: state.cardState
+    cardPagesConfig: state.cardPages,
+    cardPagesData: state.cardPagesData
   }
 }
 

75cbc5bae267d98720c73653255d7bba3005b6c5
Joakim Bording <joakim@netliferesearch.com>
2015-11-07 17:54:48 +0100

    Avoid overflow clipping of hoverbox by changing direction of arrow based on fixed edge limits of the container

--- components/elements/Hoverbox.jsx
+++ components/elements/Hoverbox.jsx
@@ -14,27 +14,34 @@ const Hoverbox = React.createClass({
   calculatePositionTo(element) {
     const offset = element.getBoundingClientRect()
 
-    let left = 0
-    let bottom = 0
-    switch (this.state.direction) {
-      case 'bottom': {
-        left = offset.left - this.state.containerRect.left + offset.width / 2
-        bottom = this.state.containerRect.height
+    const left = offset.left - this.state.containerRect.left + offset.width / 2
+    const bottom = this.state.containerRect.height
                  - (offset.top - this.state.containerRect.top)
-        break
-      }
-      case 'right': {
-        break
-      }
-      case 'left': {
-        break
-      }
-      default: {
-        left = 0
-        //top = 0
-      }
-    }
     return {left, bottom}
+  },
+
+  calculateDirection(element) {
+    const horizontalMargin = 150
+    const verticalMargin = 120
+
+    const offset = element.getBoundingClientRect()
+    const left = offset.left - this.state.containerRect.left + offset.width / 2
+    const right = this.state.containerRect.right - offset.right + offset.width / 2
+    const top = offset.top - this.state.containerRect.top
+
+    if (top < verticalMargin) {
+      if (left < horizontalMargin) {
+        return 'top-left'
+      } else if (right < horizontalMargin) {
+        return 'top-right'
+      }
+      return 'top'
+    } else if (left < horizontalMargin) {
+      return 'left'
+    } else if (right < horizontalMargin) {
+      return 'right'
+    }
+    return 'bottom'
   },
 
   render() {
@@ -45,19 +52,29 @@ const Hoverbox = React.createClass({
     }
 
     const styleClasses = ['hover-box']
-    /*
-    switch (this.state.direction) {
-      case 'left':
-        styleClasses.push('hover-box--left-edge')
-        break
-      case 'right':
-        styleClasses.push('hover-box--right-edge')
-        break
-      default:
-    }
-    */
-
     if (this.state.el) {
+
+      const direction = this.calculateDirection(this.state.el)
+
+      switch (direction) {
+        case 'left':
+          styleClasses.push('hover-box--left')
+          break
+        case 'right':
+          styleClasses.push('hover-box--right')
+          break
+        case 'top':
+          styleClasses.push('hover-box--top')
+          break
+        case 'top-right':
+          styleClasses.push('hover-box--top-right')
+          break
+        case 'top-left':
+          styleClasses.push('hover-box--top-left')
+          break
+        default:
+      }
+
       Object.assign(style, {
         display: 'block'
       }, this.calculatePositionTo(this.state.el))

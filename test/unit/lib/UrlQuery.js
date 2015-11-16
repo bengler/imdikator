4e3b90fc79dc7fcb2a32fc10ff91ee93a62cdfcd
Bjørge Næss <bjoerge@gmail.com>
2015-11-16 13:08:18 +0100

    Handle empty comparison regions in url query

--- test/unit/lib/UrlQuery.js
+++ test/unit/lib/UrlQuery.js
@@ -17,9 +17,30 @@ describe('UrlQuery', () => {
         dimensions: [{name: 'kjonn', variables: ['1']}],
         year: ['2015']
       }
+
+      const actulParts = UrlQuery.stringify(query).split(';')
+
+      ;[
+        '$cmp:K0104,K0105,K0106,K0219,K0220,K0228,K0230,K0231,K0235,K0602,K1001,K1102,K1103,K1201,K1601',
+        'kjonn:1',
+        '$u:personer',
+        '$y:2015'
+      ].forEach(expectedPart => assert.include(actulParts, expectedPart))
+
+    })
+
+    it('leaves out the comparisonRegions part from the stringified if no comparisonRegions', () => {
+      const query = {
+        unit: ['personer'],
+        region: 'K0301',
+        tableName: 'intro_deltakere',
+        comparisonRegions: [],
+        dimensions: [{name: 'kjonn', variables: ['1']}],
+        year: ['2015']
+      }
       assert.equal(
         UrlQuery.stringify(query),
-        '$cmp:K0104,K0105,K0106,K0219,K0220,K0228,K0230,K0231,K0235,K0602,K1001,K1102,K1103,K1201,K1601;$u:personer;$y:2015;kjonn:1'
+        '$u:personer;$y:2015;kjonn:1'
       )
     })
 
@@ -36,6 +57,16 @@ describe('UrlQuery', () => {
           'K0104', 'K0105', 'K0106', 'K0219', 'K0220', 'K0228', 'K0230', 'K0231',
           'K0235', 'K0602', 'K1001', 'K1102', 'K1103', 'K1201', 'K1601'
         ],
+        dimensions: [{name: 'kjonn', variables: ['1']}],
+        year: ['2015']
+      })
+    })
+    it('parses a query with no comparisonRegions to an empty array', () => {
+      const query = '$u:personer;$y:2015;kjonn:1'
+
+      assert.deepEqual(UrlQuery.parse(query), {
+        unit: ['personer'],
+        comparisonRegions: [],
         dimensions: [{name: 'kjonn', variables: ['1']}],
         year: ['2015']
       })

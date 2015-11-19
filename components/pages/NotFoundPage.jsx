33e20220cc59f0b7d1dfe644e28cf686bb6d8fb4
Joakim Bording <joakim@netliferesearch.com>
2015-11-19 16:32:46 +0100

    Updated content of NotFoundPage to contain a title, ingress and a region search

--- components/pages/NotFoundPage.jsx
+++ components/pages/NotFoundPage.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import * as ImdiPropTypes from '../proptypes/ImdiPropTypes'
+import RegionQuickSwitch from '../containers/RegionQuickSwitch'
 
 export default class NotFoundPage extends Component {
   static propTypes = {
@@ -8,9 +9,21 @@ export default class NotFoundPage extends Component {
 
   render() {
     return (
-      <div>
-        Denne siden fant vi visst ikke
-      </div>
+      <main className="page">
+        <div className="page__content">
+          <div className="wrapper">
+            <div className="row">
+              <div className="col--main">
+                <header>
+                  <h1>Siden ble ikke funnet</h1>
+                </header>
+                <div className="ingress">Kanskje den har flyttet til Norge og fått seg et bedre hjem i en kommune?</div>
+                <RegionQuickSwitch/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     )
   }
 }

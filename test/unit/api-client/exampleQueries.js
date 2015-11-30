17d8a3e1b1d2ea2b95fa2ab59c0c5b550b8e10b7
Bjørge Næss <bjoerge@gmail.com>
2015-11-30 16:58:38 +0100

    Add support for compare feature in query endpoint

--- test/unit/api-client/exampleQueries.js
+++ test/unit/api-client/exampleQueries.js
@@ -42,5 +42,53 @@ export default [
       },
       include: ['innvkat5', 'kjonn', 'aar', 'kommuneNr', 'enhet']
     }
+  },
+  {
+    test: 'A query with regions to compare with',
+    query: {
+      tableName: 'befolkninghovedgruppe',
+      region: 'K0101',
+      unit: ['personer'],
+      comparisonRegions: ['B000301', 'B030101', 'K0102', 'K0103'],
+      dimensions: [
+        {
+          name: 'innvkat5',
+          variables: [
+            'alle',
+            'innvandrere',
+            'bef_u_innv_og_norskf',
+            'norskfodte_m_innvf'
+          ]
+        },
+        {
+          name: 'kjonn',
+          variables: [
+            '0',
+            '1'
+          ]
+        },
+        {
+          name: 'aar',
+          variables: [
+            '2015'
+          ]
+        }
+      ]
+    },
+    expect: {
+      tableName: 'befolkninghovedgruppe',
+      conditions: {
+        aar: ['2015'],
+        innvkat5: ['alle', 'innvandrere', 'bef_u_innv_og_norskf', 'norskfodte_m_innvf'],
+        kjonn: ['0', '1'],
+        enhet: ['personer'],
+        kommuneNr: ['0101']
+      },
+      compare: {
+        bydelNr: ['000301', '030101'],
+        kommuneNr: ['0102', '0103']
+      },
+      include: ['innvkat5', 'kjonn', 'aar', 'kommuneNr', 'bydelNr', 'enhet']
+    }
   }
 ]

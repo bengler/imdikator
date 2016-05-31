91a40dc6e103618b55d8a1d50ac5c4d8b4ad536c
Bjørge Næss <bjoerge@gmail.com>
2016-05-31 12:16:50 +0200

    Prevent default on clicks on active chart view mode. Fixes IMDI-235

--- components/elements/ChartViewModeSelect.jsx
+++ components/elements/ChartViewModeSelect.jsx
@@ -1,6 +1,8 @@
 import React, {Component, PropTypes} from 'react'
 import preventDefault from 'prevent-default'
 
+const defaultPrevented = preventDefault(() => false)
+
 const CHART_MODES = [
   {
     name: 'chart',
@@ -26,7 +28,7 @@ export default class ChartModeSelect extends Component {
           {
             CHART_MODES.map(mode => {
               if (mode.name === selectedMode) {
-                return <a href="" key={mode.name} className="tabs-mini__link tabs-mini__link--current">{mode.title}</a>
+                return <a href="" key={mode.name} onClick={defaultPrevented} className="tabs-mini__link tabs-mini__link--current">{mode.title}</a>
               }
               return (
                 <a href=""

ab145c331b65749e37632ed593254683ed228674
Bjørge Næss <bjoerge@gmail.com>
2016-05-31 14:14:52 +0200

    Work around bug in IIS that doesn't tackle plus characters in url. Should fix IMDI-202

--- test/unit/lib/UrlQuery.js
+++ test/unit/lib/UrlQuery.js
@@ -18,15 +18,32 @@ describe('UrlQuery', () => {
         year: ['2015']
       }
 
-      const actulParts = UrlQuery.stringify(query).split(';')
-
+      const actualParts = UrlQuery.stringify(query).split(';')
       ;[
-        '$cmp=K0104,K0105,K0106,K0219,K0220,K0228,K0230,K0231,K0235,K0602,K1001,K1102,K1103,K1201,K1601',
+        '$cmp=K0104%2CK0105%2CK0106%2CK0219%2CK0220%2CK0228%2CK0230%2CK0231%2CK0235%2CK0602%2CK1001%2CK1102%2CK1103%2CK1201%2CK1601',
         'kjonn=1',
         '$u=personer',
         '$y=2015'
-      ].forEach(expectedPart => assert.include(actulParts, expectedPart))
+      ].forEach(expectedPart => assert.include(actualParts, expectedPart))
 
+    })
+
+    it('stringifies a query, encoding plus characters correctly', () => {
+
+      // Due to a bug and/or misconfiguration in the server IIS where this is served from,
+      // we need to replace regular plus-signs with full-width plus	signs (U+FF0B)
+      const query = {
+        unit: ['personer'],
+        region: 'K0301',
+        comparisonRegions: [],
+        tableName: 'intro_deltakere',
+        dimensions: [{name: 'botid5', variables: ['0_4', '5_9', '10+']}],
+        year: ['2015']
+      }
+
+      const actualParts = UrlQuery.stringify(query).split(';')
+
+      assert.include(actualParts, 'botid5=0_4%2C5_9%2C10%EF%BC%8B')
     })
 
     it('leaves out the comparisonRegions part from the stringified if no comparisonRegions', () => {

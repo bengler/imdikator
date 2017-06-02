f32f1dfb9b8055678b092bdec15c34dfdbccd6d9
bard <bnhovde@gmail.com>
2017-06-02 10:26:49 +0200

    FIX - updated node URL

--- config/index.js
+++ config/index.js
@@ -14,8 +14,8 @@ const globalConfig = (typeof window !== 'undefined' && window[API_GLOBAL] || {})
 export default defaults({
   env,
   port: process.env.PORT,
-  nodeApiHost: 'localhost:8080',
-  // nodeApiHost: 'imdikator-node-devtest.azurewebsites.net',
+  // nodeApiHost: 'localhost:8080',
+  nodeApiHost: 'imdikator-node-devtest.azurewebsites.net',
   apiHost: globalConfig.apiHost,
   contentApiHost: globalConfig.contentApiHost,
   reduxDevTools: env == 'development' && !['0', 'false'].includes(process.env.REDUX_DEVTOOLS),

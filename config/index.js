4942e030a80ea7e380acd36959f678de7289e81b
bard <bnhovde@gmail.com>
2017-08-14 16:42:49 +0200

    local testing

--- config/index.js
+++ config/index.js
@@ -14,8 +14,9 @@ const globalConfig = (typeof window !== 'undefined' && window[API_GLOBAL] || {})
 export default defaults({
   env,
   port: process.env.PORT,
-  // nodeApiHost: 'localhost:8080',
-  nodeApiHost: 'atindikatornode.azurewebsites.net',
+  nodeApiHost: 'localhost:8080',
+  // nodeApiHost: 'imdikator-node-devtest.azurewebsites.net',
+  // nodeApiHost: 'atindikatornode.azurewebsites.net',
   apiHost: globalConfig.apiHost,
   contentApiHost: globalConfig.contentApiHost,
   reduxDevTools: env == 'development' && !['0', 'false'].includes(process.env.REDUX_DEVTOOLS),

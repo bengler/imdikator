021a2b4ea0860f39b2a153d99f60e2743ffe5f11
Tobias Goulden Schultz <tobias.g.schultz@gmail.com>
2018-04-16 12:36:23 +0200

    feat: make file parser to swap semis with commas

---
+++ import/swapSemiWithComma.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// imdi delivers faulty format with duplicate values
+// this script will solve that.
+
+var fs = require('fs')
+var ora = require('ora')
+var eol = require('eol')
+var chalk = require('chalk')
+
+// show text in terminal saying script has started
+var spinner = ora('working...\n')
+spinner.start()
+
+var importPath = './import/semi.csv'
+var exportPath = './import/comma.csv'
+
+var newFileContent = '' // holds the new content to be exported
+
+// read the file
+fs.readFile(importPath, function(err, buff) {
+  // get all text from the file
+  var fileContent = buff.toString()
+
+  // split the text on newline \n
+  var arrayOfContent = eol.split(fileContent)
+
+  // handle each line
+  arrayOfContent.forEach(function(item, index) {
+    // skip the first and last line
+    if (index !== 0 && index != arrayOfContent.length) {
+      
+      // split line into array of chars
+      var values = item.split('')
+
+      // replace ; with ,
+      for (var v = 0; v < values.length; v++) {
+        if (values[v] === ';') {
+          values[v] = ','
+        }
+      }
+
+      // add parsed line to newFileContent
+      values = values.join('')
+      newFileContent += `${values}\n`
+    }
+  })
+
+  // create new file with parsed stuff
+  fs.writeFile(exportPath, newFileContent, function(error) {
+  
+    if (error) throw error
+    console.log(chalk.cyan('  Successfully overwrote previous script\n'))
+  })
+  
+  // stop spinning and say that we're finished parsing
+  console.log(chalk.cyan('  Successfully parsed script\n'))
+  spinner.stop()
+})
+
+

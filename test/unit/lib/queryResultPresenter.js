94dac82e8f023b01e280b44e6f0ab7a68e2176f9
Zheng Wang <mooseinthefur@gmail.com>
2018-06-13 13:42:11 +0200

    endret text

--- test/unit/lib/queryResultPresenter.js
+++ test/unit/lib/queryResultPresenter.js
@@ -1,5 +1,5 @@
-import {assert} from 'chai'
-import {queryResultPresenter as originalQueryResultPresenter} from '../../../lib/queryResultPresenter'
+import { assert } from 'chai'
+import { queryResultPresenter as originalQueryResultPresenter } from '../../../lib/queryResultPresenter'
 
 const QUERY = {
   tableName: 'befolkninghovedgruppe',
@@ -7,8 +7,8 @@ const QUERY = {
   unit: ['prosent'],
   region: 'K0301',
   dimensions: [
-    {name: 'innvkat5', variables: ['innvandrere']},
-    {name: 'kjonn', variables: ['0', '1']}
+    { name: 'innvkat5', variables: ['innvandrere'] },
+    { name: 'kjonn', variables: ['0', '1'] }
   ]
 }
 
@@ -46,7 +46,7 @@ describe('queryResultPresenter', () => {
         }
       }
     })
-    assert.deepEqual(pres.dimensions, [{name: 'kjonn', variables: []}])
+    assert.deepEqual(pres.dimensions, [{ name: 'kjonn', variables: [] }])
   })
 
   it('collects dimensions specified in the query', () => {
@@ -65,15 +65,15 @@ describe('queryResultPresenter', () => {
       ]
     }
 
-    const pres = queryResultPresenter(query, [], {chartKind: 'bar'})
-    assert.deepEqual(pres.dimensions, [{name: 'innvkat5', variables: []}, {name: 'kjonn', variables: ['0', '1']}])
+    const pres = queryResultPresenter(query, [], { chartKind: 'bar' })
+    assert.deepEqual(pres.dimensions, [{ name: 'innvkat5', variables: [] }, { name: 'kjonn', variables: ['0', '1'] }])
   })
 
   it('removes time dimension if the query only has one distinct year', () => {
     const query = Object.assign({}, QUERY, {
       year: ['2015']
     })
-    const pres = queryResultPresenter(query, [], {chartKind: 'bar'})
+    const pres = queryResultPresenter(query, [], { chartKind: 'bar' })
     assert(!pres.dimensions.includes('aar'))
   })
 
@@ -84,89 +84,89 @@ describe('queryResultPresenter', () => {
       comparisonRegions: ['F06'],
       unit: ['prosent'],
       dimensions: [
-        {name: 'innvkat5', variables: ['innvandrere']},
-        {name: 'kjonn', variables: ['0', '1']}
+        { name: 'innvkat5', variables: ['innvandrere'] },
+        { name: 'kjonn', variables: ['0', '1'] }
       ]
     }
-    const pres = queryResultPresenter(query, [], {chartKind: 'pyramid'})
+    const pres = queryResultPresenter(query, [], { chartKind: 'pyramid' })
     assert.deepEqual(pres.dimensions, [
-      {name: 'region', variables: []},
-      {name: 'innvkat5', variables: ['innvandrere']},
-      {name: 'kjonn', variables: ['0', '1']}
+      { name: 'region', variables: [] },
+      { name: 'innvkat5', variables: ['innvandrere'] },
+      { name: 'kjonn', variables: ['0', '1'] }
     ])
   })
 
-  ;['bar', 'stackedBar', 'pyramid'].forEach(chartKind => {
-    it(`sorts aar dimension last for ${chartKind}`, () => {
-      const query = Object.assign({}, QUERY, {
-        dimensions: [
-          {name: 'innvkat5', variables: ['innvandrere']}
-        ]
+    ;['bar', 'stackedBar', 'pyramid'].forEach(chartKind => {
+      it(`sorts aar dimension last for ${chartKind}`, () => {
+        const query = Object.assign({}, QUERY, {
+          dimensions: [
+            { name: 'innvkat5', variables: ['innvandrere'] }
+          ]
+        })
+        const pres = queryResultPresenter(query, [], { chartKind: chartKind })
+        assert.deepEqual(pres.dimensions.slice(-1)[0], { name: 'aar', variables: [] })
       })
-      const pres = queryResultPresenter(query, [], {chartKind: chartKind})
-      assert.deepEqual(pres.dimensions.slice(-1)[0], {name: 'aar', variables: []})
     })
-  })
 
-  ;['line', 'stackedArea'].forEach(chartKind => {
-    it(`always includes year as last dimension ${chartKind}`, () => {
-      const query = Object.assign({}, QUERY, {
-        dimensions: [
-          {name: 'innvkat5', variables: ['innvandrere']}
-        ]
+    ;['line', 'stackedArea'].forEach(chartKind => {
+      it(`always includes year as last dimension ${chartKind}`, () => {
+        const query = Object.assign({}, QUERY, {
+          dimensions: [
+            { name: 'innvkat5', variables: ['innvandrere'] }
+          ]
+        })
+        const pres = queryResultPresenter(query, [], { chartKind: chartKind })
+        assert.deepEqual(pres.dimensions.slice(-1)[0], { name: 'aar', variables: [] })
       })
-      const pres = queryResultPresenter(query, [], {chartKind: chartKind})
-      assert.deepEqual(pres.dimensions.slice(-1)[0], {name: 'aar', variables: []})
     })
-  })
 
   it('figures out the unit to use when presenting based on the query', () => {
     const qu = Object.assign({}, QUERY, {
       unit: ['kustom']
     })
-    const pres = queryResultPresenter(qu, [], {chartKind: 'bar'})
+    const pres = queryResultPresenter(qu, [], { chartKind: 'bar' })
     assert.equal(pres.unit, 'kustom')
   })
 
   it('includes the query result as a rows property', () => {
-    const result = [{something: 'here'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ something: 'here' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.deepEqual(pres.rows, result)
   })
 
   it('parses tabellvariabel into value property', () => {
-    const result = [{tabellvariabel: '29'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ tabellvariabel: '29' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.equal(pres.rows[0].value, 29)
   })
 
   it('marks anonymized data', () => {
-    const result = [{tabellvariabel: ':'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ tabellvariabel: ':' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.property(pres.rows[0], 'anonymized')
     assert.ok(pres.rows[0].anonymized)
     assert.equal(pres.rows[0].value, 4)
   })
 
   it('anonymized data has formattedValue set', () => {
-    const result = [{tabellvariabel: ':'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ tabellvariabel: ':' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.property(pres.rows[0], 'formattedValue')
-    assert.equal(pres.rows[0].formattedValue, 'Skjult av personvernshensyn')
+    assert.equal(pres.rows[0].formattedValue, 'Skjult')
   })
 
   it('marks missing data', () => {
-    const result = [{tabellvariabel: '.'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ tabellvariabel: '.' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.property(pres.rows[0], 'missingData')
     assert.ok(pres.rows[0].missingData)
     assert.equal(pres.rows[0].value, 0)
   })
 
   it('missing data has formattedValue set', () => {
-    const result = [{tabellvariabel: '.'}]
-    const pres = queryResultPresenter(QUERY, result, {chartKind: 'bar'})
+    const result = [{ tabellvariabel: '.' }]
+    const pres = queryResultPresenter(QUERY, result, { chartKind: 'bar' })
     assert.property(pres.rows[0], 'formattedValue')
-    assert.equal(pres.rows[0].formattedValue, 'Mangler data')
+    assert.equal(pres.rows[0].formattedValue, 'Mangler')
   })
 })

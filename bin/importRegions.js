877dee7ec23d6bcb14a88df6a84edccec5b62807
Zheng Wang <mooseinthefur@gmail.com>
2018-06-18 15:51:53 +0200

    fix name import for nærningsregion

--- bin/importRegions.js
+++ bin/importRegions.js
@@ -1,165 +1,192 @@
-import fs from 'fs'
-import RxNode from 'rx-node'
-import {pick} from 'lodash'
-import csv from 'csv-parse'
-import assert from 'assert'
-import pify from 'pify'
-import {prefixify} from '../lib/regionUtil'
+import fs from 'fs';
+import RxNode from 'rx-node';
+import { pick } from 'lodash';
+import csv from 'csv-parse';
+import assert from 'assert';
+import pify from 'pify';
+import { prefixify } from '../lib/regionUtil';
 
-const writeFile = pify(fs.writeFile)
+const writeFile = pify(fs.writeFile);
 
-const log = console.log.bind(console) // eslint-disable-line
+const log = console.log.bind(console); // eslint-disable-line
 
-const CSV_FILE_FYLKER_KOMMUNER = './import/regioninndeling-fylker-kommuner.csv'
-const CSV_FILE_KOMMUNER_BYDELER = './import/regioninndeling-kommuner-bydeler.csv'
-const OUTPUT_FILE_BYDELER = './data/bydeler.json'
-const OUTPUT_FILE_KOMMUNER = './data/kommuner.json'
-const OUTPUT_FILE_FYLKER = './data/fylker.json'
-const OUTPUT_FILE_NÆRINGSREGIONER = './data/naeringsregioner.json'
+const CSV_FILE_FYLKER_KOMMUNER = './import/regioninndeling-fylker-kommuner.csv';
+const CSV_FILE_KOMMUNER_BYDELER =
+  './import/regioninndeling-kommuner-bydeler.csv';
+const OUTPUT_FILE_BYDELER = './data/bydeler.json';
+const OUTPUT_FILE_KOMMUNER = './data/kommuner.json';
+const OUTPUT_FILE_FYLKER = './data/fylker.json';
+const OUTPUT_FILE_NÆRINGSREGIONER = './data/naeringsregioner.json';
 
 function leftPad(str, padding, len) {
-  let pad = ''
+  let pad = '';
   while (pad.length < len) {
-    pad += padding
+    pad += padding;
   }
-  return pad.substring(str.length) + str
+  return pad.substring(str.length) + str;
 }
 
 const parsedRegions = csvToObjects(CSV_FILE_FYLKER_KOMMUNER)
   .map(padKeys('0', 4, 'Kommunenr'))
   .map(padKeys('0', 2, 'Fylkenr'))
-  .map(padKeys('0', 2, 'Næringsregionnr'))
+  .map(padKeys('0', 2, 'Næringsregionnr'));
 
 const fylker = parsedRegions
   .distinct(region => region.Fylkenr)
   .map(pickKeys('Fylkenr', 'Fylkenavn', 'Validity'))
-  .map(renameKeys({
-    Fylkenr: 'code',
-    Fylkenavn: 'name'
-  }))
+  .map(
+    renameKeys({
+      Fylkenr: 'code',
+      Fylkenavn: 'name'
+    })
+  )
   .map(fylke => {
     return Object.assign({}, fylke, {
       type: 'county',
       prefixedCode: prefixify('county', fylke.code)
-    })
+    });
   })
   //.filter(region => !region.name.includes('Uoppgitt'))
   .toArray()
-  .flatMap(serializeTo(OUTPUT_FILE_FYLKER))
+  .flatMap(serializeTo(OUTPUT_FILE_FYLKER));
 
 const kommuner = parsedRegions
   .distinct(region => region.Kommunenr)
-  .map(pickKeys('Kommunenr', 'Kommunenavn', 'Fylkenr', 'IMDiRegion', 'Næringsregionnr', 'Sentralitet_nr_2008', 'Sentralitet_kat_2008'))
-  .map(renameKeys({
-    Kommunenr: 'code',
-    Kommunenavn: 'name',
-    Fylkenr: 'countyCode',
-    IMDiRegion: 'imdiRegion',
-    Næringsregionnr: 'commerceRegionCode',
-    /* eslint-disable camelcase */
-    Sentralitet_nr_2008: 'centralityNumber',
-    Sentralitet_kat_2008: 'centralityName'
-    /* eslint-enable camelcase */
-  }))
+  .map(
+    pickKeys(
+      'Kommunenr',
+      'Kommunenavn',
+      'Fylkenr',
+      'IMDiRegion',
+      'Næringsregionnr',
+      'Sentralitet_nr_2008',
+      'Sentralitet_kat_2008'
+    )
+  )
+  .map(
+    renameKeys({
+      Kommunenr: 'code',
+      Kommunenavn: 'name',
+      Fylkenr: 'countyCode',
+      IMDiRegion: 'imdiRegion',
+      Næringsregionnr: 'commerceRegionCode',
+      /* eslint-disable camelcase */
+      Sentralitet_nr_2008: 'centralityNumber',
+      Sentralitet_kat_2008: 'centralityName'
+      /* eslint-enable camelcase */
+    })
+  )
   .map(kommune => {
     return Object.assign({}, kommune, {
       name: kommune.name === 'Oslo kommune' ? 'Oslo' : kommune.name,
       type: 'municipality',
       prefixedCode: prefixify('municipality', kommune.code)
-    })
+    });
   })
   .toArray()
-  .flatMap(serializeTo(OUTPUT_FILE_KOMMUNER))
+  .flatMap(serializeTo(OUTPUT_FILE_KOMMUNER));
 
 const naeringsregioner = parsedRegions
   .distinct(region => region['Næringsregionnr'])
-  .map(pickKeys('Næringsregionnr', 'Næringsregion_ navn', 'Kommunenr', 'Fylkenr'))
-  .map(renameKeys({
-    Næringsregionnr: 'code',
-    'Næringsregion_ navn': 'name',
-    Kommunenr: 'municipalityCode',
-    Fylkenr: 'countyCode'
-  }))
+  .map(
+    pickKeys('Næringsregionnr', 'Næringsregion_navn', 'Kommunenr', 'Fylkenr')
+  )
+  .map(
+    renameKeys({
+      Næringsregionnr: 'code',
+      Næringsregion_navn: 'name',
+      Kommunenr: 'municipalityCode',
+      Fylkenr: 'countyCode'
+    })
+  )
   .map(naeringsregion => {
     return Object.assign({}, naeringsregion, {
       type: 'commerceRegion',
       prefixedCode: prefixify('commerceRegion', naeringsregion.code)
-    })
+    });
   })
   .toArray()
-  .flatMap(serializeTo(OUTPUT_FILE_NÆRINGSREGIONER))
+  .flatMap(serializeTo(OUTPUT_FILE_NÆRINGSREGIONER));
 
 const bydeler = csvToObjects(CSV_FILE_KOMMUNER_BYDELER)
   .distinct(region => region.bydelsnr)
   .map(padKeys('0', 6, 'bydelsnr'))
   .map(pickKeys('bydelsnr', 'bydelsnavn', 'Kommunenr'))
-  .map(renameKeys({
-    bydelsnr: 'code',
-    bydelsnavn: 'name',
-    Kommunenr: 'municipalityCode'
-  }))
+  .map(
+    renameKeys({
+      bydelsnr: 'code',
+      bydelsnavn: 'name',
+      Kommunenr: 'municipalityCode'
+    })
+  )
   .map(bydel => {
     return Object.assign({}, bydel, {
       type: 'borough',
       prefixedCode: prefixify('borough', bydel.code)
-    })
+    });
   })
   .toArray()
-  .flatMap(serializeTo(OUTPUT_FILE_BYDELER))
+  .flatMap(serializeTo(OUTPUT_FILE_BYDELER));
 
-naeringsregioner.subscribe(res => log('Wrote %s næringsregioner to %s', res.entries.length, res.file))
-kommuner.subscribe(res => log('Wrote %s kommuner to %s', res.entries.length, res.file))
-bydeler.subscribe(res => log('Wrote %s bydeler to %s', res.entries.length, res.file))
-fylker.subscribe(res => log('Wrote %s fylker to %s', res.entries.length, res.file))
+naeringsregioner.subscribe(res =>
+  log('Wrote %s næringsregioner to %s', res.entries.length, res.file)
+);
+kommuner.subscribe(res =>
+  log('Wrote %s kommuner to %s', res.entries.length, res.file)
+);
+bydeler.subscribe(res =>
+  log('Wrote %s bydeler to %s', res.entries.length, res.file)
+);
+fylker.subscribe(res =>
+  log('Wrote %s fylker to %s', res.entries.length, res.file)
+);
 
 // A few helper functions
 function csvToObjects(file) {
-  const rows = RxNode.fromReadableStream(fs.createReadStream(file).pipe(csv()))
-  return rows
-    .skip(1)
-    .withLatestFrom(rows.take(1), (row, header) => {
-      return row.reduce((rowObj, cell, i) => {
-        // Map header[i] => row[i]
-        rowObj[header[i]] = cell
-        return rowObj
-      }, {})
-    })
+  const rows = RxNode.fromReadableStream(fs.createReadStream(file).pipe(csv()));
+  return rows.skip(1).withLatestFrom(rows.take(1), (row, header) => {
+    return row.reduce((rowObj, cell, i) => {
+      // Map header[i] => row[i]
+      rowObj[header[i]] = cell;
+      return rowObj;
+    }, {});
+  });
 }
 
 function renameKeys(keyNamesMap) {
-  return function (object) {
-    const knownKeyNames = Object.keys(keyNamesMap)
+  return function(object) {
+    const knownKeyNames = Object.keys(keyNamesMap);
     return Object.keys(object).reduce((renamed, key) => {
       if (!knownKeyNames.includes(key)) {
-        throw new Error(`Don't know what to rename the key '${key}' to`)
+        throw new Error(`Don't know what to rename the key '${key}' to`);
       }
-      renamed[keyNamesMap[key]] = object[key]
-      return renamed
-    }, {})
-  }
+      renamed[keyNamesMap[key]] = object[key];
+      return renamed;
+    }, {});
+  };
 }
 
 function pickKeys(...keys) {
-  return function (object) {
-    return pick(object, ...keys)
-  }
+  return function(object) {
+    return pick(object, ...keys);
+  };
 }
 
 function padKeys(padding, len, ...keys) {
-  return function (object) {
+  return function(object) {
     const padded = keys.reduce((acc, key) => {
-      assert(object[key], `Missing key ${key} in ${JSON.stringify(object)}`)
-      acc[key] = leftPad(object[key], padding, len)
-      return acc
-    }, {})
-    return Object.assign({}, object, padded)
-  }
+      assert(object[key], `Missing key ${key} in ${JSON.stringify(object)}`);
+      acc[key] = leftPad(object[key], padding, len);
+      return acc;
+    }, {});
+    return Object.assign({}, object, padded);
+  };
 }
 
 function serializeTo(file) {
   return function serialize(entries) {
     return writeFile(file, JSON.stringify(entries, null, 2)).then(() => {
-      return {file, entries}
-    })
-  }
+      return { file, entries };
+    });
+  };
 }
